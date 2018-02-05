1fe01a5b69debcfe04426230c0c49877ec44186d
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 13:57:23 +0100

    dev(bash_profile.prompt.sh): use git_extract_current_branch()

--- bash/includes/bash_profile.prompt.sh
+++ bash/includes/bash_profile.prompt.sh
@@ -3,8 +3,7 @@
 HOST='\033[02;36m\]\h'; HOST=' '$HOST
 TIME='\033[01;31m\]\t \033[01;32m\]'
 LOCATION=' \033[01;94m\]`pwd | sed "s#\(/[^/]\{1,\}/[^/]\{1,\}/[^/]\{1,\}/\).*\(/[^/]\{1,\}/[^/]\{1,\}\)/\{0,1\}#\1_\2#g"`'
-BRANCH=' \033[01;33m\]BRANCH\[\033[00m\]\n\$ '
+BRANCH=' \033[01;33m\]($(git_extract_current_branch))\[\033[00m\]\n\$ '
 PS1=$TIME$USER$HOST$LOCATION$BRANCH
 PS2='\[\033[01;36m\]>'
 
-hello
