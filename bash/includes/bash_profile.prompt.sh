bb3818ff7f748ca8155bfe5c568dff7a03cc615f
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 14:13:27 +0100

    dev.refactor(bash_profile.prompt.sh): use " instead of '

--- bash/includes/bash_profile.prompt.sh
+++ bash/includes/bash_profile.prompt.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-HOST='\033[02;36m\]\h';
-TIME='\033[01;31m\]\t \033[01;32m\]'
-LOCATION='\033[01;94m\]$(get_current_working_dir)'
-BRANCH='\033[01;33m\]($(git_extract_current_branch))\[\033[00m\]\n\$ '
+HOST="\033[02;36m\]\h";
+TIME="\033[01;31m\]\t \033[01;32m\]"
+LOCATION="\033[01;94m\]$(get_current_working_dir)"
+BRANCH="\033[01;33m\]($(git_extract_current_branch))\[\033[00m\]\n\$"
+
 PS1="$TIME$USER@$HOST $LOCATION $BRANCH"
-PS2='\[\033[01;36m\]>'
+PS2="\[\033[01;36m\]>"
 
