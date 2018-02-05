97fdaf0a74090e0d78b0cc9a565dca4a6169c841
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 14:14:51 +0100

    dev.refactor(bash_profile.prompt.sh): get_current_git_branch > renaming

--- bash/includes/bash_profile.prompt.sh
+++ bash/includes/bash_profile.prompt.sh
@@ -3,7 +3,7 @@
 HOST="\033[02;36m\]\h";
 TIME="\033[01;31m\]\t \033[01;32m\]"
 LOCATION="\033[01;94m\]$(get_current_working_dir)"
-BRANCH="\033[01;33m\]($(git_extract_current_branch))\[\033[00m\]\n\$"
+BRANCH="\033[01;33m\]($(get_current_git_branch))\[\033[00m\]\n\$"
 
 PS1="$TIME$USER@$HOST $LOCATION $BRANCH"
 PS2="\[\033[01;36m\]>"
