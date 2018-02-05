201a360a26d812b9198bdd1def572084b92c5682
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 17:17:29 +0100

    dev(bash_profile.sh): include bash.git.sh

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -14,4 +14,6 @@ echo "# Bash profile extension"
 source ~/git-bash-workflow/bash/includes/bash.variables.sh
 source ~/git-bash-workflow/bash/includes/bash.formatting.sh
 source ~/git-bash-workflow/bash/includes/bash.functions.sh
+
+source ~/git-bash-workflow/bash/includes/bash.git.sh
 source ~/git-bash-workflow/bash/includes/bash.prompt.sh
