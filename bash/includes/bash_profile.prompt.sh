3ba1dc8449eb844137fda8fad836b3f600b7502e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 14:11:39 +0100

    dev.refactor(bash_profile.prompt.sh): use get_current_working_dir()

--- bash/includes/bash_profile.prompt.sh
+++ bash/includes/bash_profile.prompt.sh
@@ -2,7 +2,7 @@
 
 HOST='\033[02;36m\]\h';
 TIME='\033[01;31m\]\t \033[01;32m\]'
-LOCATION='\033[01;94m\]`pwd | sed "s#\(/[^/]\{1,\}/[^/]\{1,\}/[^/]\{1,\}/\).*\(/[^/]\{1,\}/[^/]\{1,\}\)/\{0,1\}#\1_\2#g"`'
+LOCATION='\033[01;94m\]$(get_current_working_dir)'
 BRANCH='\033[01;33m\]($(git_extract_current_branch))\[\033[00m\]\n\$ '
 PS1="$TIME$USER@$HOST $LOCATION $BRANCH"
 PS2='\[\033[01;36m\]>'
