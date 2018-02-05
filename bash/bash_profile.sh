8a74c69bab2f930d0b7265947f25996e92559b67
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 15:47:31 +0100

    dev(bash_profile.sh): include bash.formatting.sh

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -12,5 +12,6 @@
 echo "# Bash profile extension"
 
 source ~/git-bash-workflow/bash/includes/bash.variables.sh
+source ~/git-bash-workflow/bash/includes/bash.formatting.sh
 source ~/git-bash-workflow/bash/includes/bash.functions.sh
 source ~/git-bash-workflow/bash/includes/bash_profile.prompt.sh
