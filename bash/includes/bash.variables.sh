92e5d585287306296708fa78cfadc41657755c27
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 15:47:48 +0100

    dev.refactor(bash.variables.sh): move formatting vars in bash.formatting.sh

--- bash/includes/bash.variables.sh
+++ bash/includes/bash.variables.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env="033[bash
-
-RED="\033[0;31m\]"
-RED_LITTLE_DARKER="\033[02;36m\]"
-GREEN="\033[0;32m\]"
-GREEN_LIGHTER="\033[01;32m\]"
-ORANGE="\033[0;33m\]"
-CYAN="\033[0;36m\]"
-BLUE="\033[02;36m\]"
-NC="\033[0m\]"
-
-BG_MAGENTA="\e[45m\]"
-BG_NC="\e[0m\]"
+#!/usr/bin/env bash
