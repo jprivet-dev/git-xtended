d0796da80d740c9ee7798de9335d0d80593ecf95
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 17:49:28 +0100

    dev.debug(bash.prompt.sh): PS1 does not refresh > insert old code

--- bash/includes/bash.prompt.sh
+++ bash/includes/bash.prompt.sh
@@ -20,6 +20,16 @@ function prompt_ps2_format {
     echo "$C_CYAN>"
 }
 
-PS1=$(prompt_ps1_format)
+#PS1=$(prompt_ps1_format)
 PS2=$(prompt_ps2_format)
+
+function _prompt_ps1_format {
+    HOST='\033[02;36m\]\h'; HOST=' '$HOST
+    TIME='\033[01;31m\]\t \033[01;32m\]'
+    LOCATION=' \033[01;94m\]`pwd | sed "s#\(/[^/]\{1,\}/[^/]\{1,\}/[^/]\{1,\}/\).*\(/[^/]\{1,\}/[^/]\{1,\}\)/\{0,1\}#\1_\2#g"`'
+    BRANCH=' \033[01;33m\]_git_branch_\[\033[00m\]\n\$ '
+    PS1=$TIME$USER$HOST$LOCATION$BRANCH
+}
+
+_prompt_ps1_format
 
