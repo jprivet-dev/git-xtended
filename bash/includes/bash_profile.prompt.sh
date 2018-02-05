a78d8bf8244662da764cae72bfb121287b9fedf5
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 15:16:19 +0100

    dev.refactor(bash_profile.prompt.sh): vars renaming

--- bash/includes/bash_profile.prompt.sh
+++ bash/includes/bash_profile.prompt.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-HOST="\033[02;36m\]\h";
-TIME="\033[01;31m\]\t \033[01;32m\]"
-LOCATION="\033[01;94m\]$(get_current_working_dir)"
-BRANCH="\033[01;33m\]($(get_current_git_branch))\[\033[00m\]\n\$"
+_PROMPT_HOST="\033[02;36m\]\h"
+_PROMPT_TIME="\033[01;31m\]\t"
+_PROMPT_USER="\033[01;32m\]$USER"
+_PROMPT_LOCATION="\033[01;94m\]$(get_current_working_dir)"
+_PROMPT_BRANCH="\033[01;33m\]($(get_current_git_branch))\[\033[00m\]"
+_PROMPT_END="\n\$ "
 
-PS1="$TIME$USER@$HOST $LOCATION $BRANCH"
+PS1="$_PROMPT_TIME $_PROMPT_USER@$_PROMPT_HOST $_PROMPT_LOCATION $_PROMPT_BRANCH$_PROMPT_END"
 PS2="\[\033[01;36m\]>"
 
