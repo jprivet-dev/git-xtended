f0e29d8792347cab087d1bc09017a293f5fd7e19
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 14:11:21 +0100

    dev(bash.functions.sh): create get_current_working_dir()

--- bash/includes/bash.functions.sh
+++ bash/includes/bash.functions.sh
@@ -7,3 +7,7 @@ function hello {
 function git_extract_current_branch {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
+
+function get_current_working_dir {
+    pwd | sed "s#\(/[^/]\{1,\}/[^/]\{1,\}/[^/]\{1,\}/\).*\(/[^/]\{1,\}/[^/]\{1,\}\)/\{0,1\}#\1_\2#g"
+}
