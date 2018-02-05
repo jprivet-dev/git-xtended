3d71cb4b8a7c736110e7edd1e66cebf75a924e7a
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-05 16:57:07 +0100

    dev.refactor(bash_profile.prompt.sh): create functions

--- bash/includes/bash_profile.prompt.sh
+++ bash/includes/bash_profile.prompt.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-_PROMPT_TIME="$C_LIGHT_RED\t"
-_PROMPT_USER="$C_LIGHT_GREEN$USER@"
-_PROMPT_HOST="$C_LIGHT_CYAN\h"
-_PROMPT_LOCATION="$C_LIGHT_BLUE$(get_current_working_dir)"
-_PROMPT_BRANCH="$C_LIGHT_YELLOW($(get_current_git_branch))"
-_PROMPT_END="$F_RESET\n\$ "
+function prompt_ps1_format {
+    _PROMPT_TIME="$C_LIGHT_RED\t"
+    _PROMPT_USER="$C_LIGHT_GREEN$USER@"
+    _PROMPT_HOST="$C_LIGHT_CYAN\h"
+    _PROMPT_LOCATION="$C_LIGHT_BLUE$(get_current_working_dir)"
 
-PS1="$_PROMPT_TIME $_PROMPT_USER$_PROMPT_HOST $_PROMPT_LOCATION $_PROMPT_BRANCH$_PROMPT_END"
-PS2="$C_CYAN>"
+    _PROMPT_BRANCH=""
+    if [[ -n "$(get_current_git_branch)" ]]; then
+        _PROMPT_BRANCH=" $C_LIGHT_YELLOW($(get_current_git_branch))"
+    fi
+
+    _PROMPT_END="$F_RESET\n\$ "
+
+    echo "$_PROMPT_TIME $_PROMPT_USER$_PROMPT_HOST $_PROMPT_LOCATION$_PROMPT_BRANCH$_PROMPT_END"
+}
+
+function prompt_ps2_format {
+    echo "$C_CYAN>"
+}
+
+PS1=$(prompt_ps1_format)
+PS2=$(prompt_ps2_format)
 
