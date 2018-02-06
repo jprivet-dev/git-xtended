91f0dcf1e0800640751fb0465243a8b198844404
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-06 23:25:23 +0100

    dev(bash.prompt.sh): prompt_ps1_format > update params

--- bash/includes/bash.prompt.sh
+++ bash/includes/bash.prompt.sh
@@ -13,8 +13,18 @@ function prompt_ps1_format {
     local _STATUS_NOT_STAGED=""
     local _STATUS_UNTRACKED=""
     local _STATUS=""
-    local _AHEAD_BEHIND="\n$(git_status_ahead_count $(git_get_current_branch)) commits ahead, $(git_status_behind_count) commit behind origin/dev"
-    local _END="$F_RESET\n\$ "
+
+    local _AHEAD="$(git_status_ahead_count $(git_get_current_branch))↑"
+    local _BEHIND_ALERT=""
+
+    if [[ "$(git_status_behind_count)" > 0 ]]; then
+        _BEHIND_ALERT="$C_ORANGE*$F_RESET"
+    fi
+
+    local _BEHIND="$_BEHIND_ALERT$(git_status_behind_count)↓"
+    local _AHEAD_BEHIND="${C_LIGHT_GRAY}$_BEHIND $_AHEAD${F_RESET}"
+
+    local _END="$_BEHIND_ALERT$F_RESET\$ "
 
     if [[ -n "$(git_get_current_branch)" ]]; then
         _BRANCH=" $C_LIGHT_YELLOW$(git_get_current_branch)"
@@ -22,17 +32,17 @@ function prompt_ps1_format {
         if [[ "$(git_get_changes_nb)" > 0 ]]; then
             _BRANCH="$_BRANCH ($(git_get_changes_nb))"
 
-            _STATUS_TO_BE_COMMITED="${C_LIGHT_GREEN}C$_STATUS_TO_BE_COMMITED_COUNT${F_RESET}"
+            _STATUS_TO_BE_COMMITED="${C_LIGHT_GREEN}${_STATUS_TO_BE_COMMITED_COUNT}c${F_RESET}"
             if [[ "$_STATUS_TO_BE_COMMITED_COUNT" > 0 ]]; then
                 _STATUS_TO_BE_COMMITED="$F_UNDERLINED$_STATUS_TO_BE_COMMITED"
             fi
 
-            _STATUS_NOT_STAGED="${C_LIGHT_RED}M$_STATUS_NOT_STAGED_COUNT${F_RESET}"
+            _STATUS_NOT_STAGED="${C_LIGHT_RED}$_STATUS_NOT_STAGED_COUNT!${F_RESET}"
             if [[ "$_STATUS_NOT_STAGED_COUNT" > 0 ]]; then
                 _STATUS_NOT_STAGED="$F_UNDERLINED$_STATUS_NOT_STAGED"
             fi
 
-            _STATUS_UNTRACKED="${C_LIGHT_RED}?$_STATUS_UNTRACKED_COUNT${F_RESET}"
+            _STATUS_UNTRACKED="${C_LIGHT_RED}$_STATUS_UNTRACKED_COUNT?${F_RESET}"
             if [[ "$_STATUS_UNTRACKED_COUNT" > 0 ]]; then
                 _STATUS_UNTRACKED="$F_UNDERLINED$_STATUS_UNTRACKED"
             fi
@@ -41,7 +51,7 @@ function prompt_ps1_format {
         fi
     fi
 
-    PS1="$_TIME $_USER$_HOST $_DIR$_BRANCH$_STATUS$_AHEAD_BEHIND$_END"
+    PS1="$_TIME $_USER$_HOST $_DIR$_BRANCH$_STATUS $_AHEAD_BEHIND\n$_END"
 }
 
 function prompt_ps2_format {
