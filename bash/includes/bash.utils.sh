37edd74486897e95870270c7a5dd054335d37afe
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-07 11:52:08 +0100

    dev(bash.utils.sh): gbw_implode > update

--- bash/includes/bash.utils.sh
+++ bash/includes/bash.utils.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
+# Example :
+# test_1="hello"
+# test_2="bonjour"
+# test_3=""
+# gbw_implode ', ' 'a' 'b' "$test_1" 3 "$test_2" 4 "$test_3" 5
+# a, b, hello, 3, bonjour, 4, 5
 function gbw_implode {
     local glue="$1"
     shift
-    echo "$*"
+    local -a pieces="($*)"
+    local implode=""
+
+    for p in "${pieces[@]}"
+    do
+        if [[ "$implode" == "" ]]; then
+            implode="$p"
+        else
+            implode="$implode$glue$p"
+        fi
+    done
+
+    echo "$implode"
 }
