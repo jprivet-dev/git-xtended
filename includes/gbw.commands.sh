784da8fe26b442e119765dd5d50cff7d58954696
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-08 22:01:57 +0100

    dev(gbw.commands.sh): create gbw_command_aliases

--- includes/gbw.commands.sh
+++ includes/gbw.commands.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-# Alias of gbw_command
+# Alias of gbw_command_parse_action
 function gbw {
-    gbw_command $@
+    gbw_command_parse_action $@
 }
 
-function gbw_command {
-    echo "Hello!"
+function gbw_command_parse_action {
+    local action=$1
+    shift
+
+    case $action in
+        aliases)
+            gbw_command_aliases $@
+        ;;
+        *)
+            echo "Unknow $action"
+        ;;
+    esac
+}
+
+function gbw_command_parse_options {
+    echo "options"
+}
+
+function gbw_command_aliases {
+    gbw_git_apply_global_config $@
 }
