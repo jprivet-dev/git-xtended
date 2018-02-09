71c55ad0dd54ee31e606e3428cc296c87c218a77
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-09 11:25:58 +0100

    dev(gbw.params.sh): add LC_ALL=C

--- includes/gbw.params.sh
+++ includes/gbw.params.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Change local
+export LC_ALL=C
+
 GBW_BRANCH_MAIN_REMOTE_DEVELOP="origin/develop"
 GBW_BRANCH_MAIN_REMOTE_RELEASE="origin/release"
 GBW_BRANCH_MAIN_REMOTE_MASTER="origin/"
