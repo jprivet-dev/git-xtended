cb99062e60260ea2b2af269608778bf2770cd2d5
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-12 22:50:03 +0100

    dev(gbw.prompt.tests.sh): fake string

--- tests/gbw.prompt.tests.sh
+++ tests/gbw.prompt.tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 function test_gbw_prompt_time {
-    echo "test_gbw_prompt_time"
+    echo "Hello test_gbw_prompt_time"
+}
+
+function test_gbw_prompt_user {
+    echo "Hello test_gbw_prompt_time"
 }
