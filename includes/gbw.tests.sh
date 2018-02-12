4b62def7b86913a4238468d4d8f1058fe4fbdbf6
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-12 23:08:02 +0100

    dev(gbw.tests.sh): create base of gbw_tests_assert_equals

--- includes/gbw.tests.sh
+++ includes/gbw.tests.sh
@@ -11,3 +11,27 @@ function gbw_tests_run_all {
         echo
     done
 }
+
+function gbw_tests_assert_equals {
+    [[ "$1" == "$2" ]] && echo "$GBW_TRUE" ||  echo "$GBW_FALSE"
+}
+
+function gbw_tests_assert {
+    local type=$1
+    shift
+
+    case $type in
+        aliases)
+            gbw_tests_assert_equals $@
+        ;;
+        *)
+            echo "Unknow assert type '$type'"
+            return
+        ;;
+    esac
+}
+
+# gbw_tests_assert_equals alias
+function assert {
+    gbw_tests_assert $@
+}
