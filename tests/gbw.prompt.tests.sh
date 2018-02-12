dc85959bee8d5f5f40e77cf303a22882e5b586c2
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-12 23:55:16 +0100

    dev(gbw.prompt.tests.sh): use assert equals

--- tests/gbw.prompt.tests.sh
+++ tests/gbw.prompt.tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function test_gbw_prompt_time {
-    echo "Hello test_gbw_prompt_time"
+    assert equals "$(gbw_prompt_time)" "\e[91m\t\e[0m"
 }
 
 function test_gbw_prompt_user {
-    echo "Hello test_gbw_prompt_time"
+    assert equals "$(gbw_prompt_user)" "\e[92m\e[0m"
 }
