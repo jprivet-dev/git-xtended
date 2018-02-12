2471b53843f64fefdab318b1a0249570081596ff
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-12 22:33:06 +0100

    dev(gbw.tests.sh): find all func test

--- includes/gbw.tests.sh
+++ includes/gbw.tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-function gbw_tests_find_all {
-    echo "Find all"
+function gbw_tests_find_all_func_test {
+    compgen -A function | grep test_
+}
+
+function gbw_tests_run_all {
+    gbw_tests_find_all_func_test
 }
