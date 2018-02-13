838a080dbc1b65b5707cff3f28347c15f9daecd8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 14:57:50 +0100

    dev(tests.sh): set git alias on "gbw_tests_fake_git"

--- tests.sh
+++ tests.sh
@@ -11,11 +11,16 @@ source ~/git-bash-workflow/src/gbw.tests.sh
 source ~/git-bash-workflow/tests/gbw.prompt.tests.sh
 source ~/git-bash-workflow/tests/gbw.git.tests.sh
 
+alias git="gbw_tests_fake_git"
+
 gbw_tests_run_all
+
+unalias git
 
 echo "##########################################"
 echo "# Switch to original Git Bash Workflow ! #";
 echo "##########################################"
 echo
+
 
 source ~/git-bash-workflow/gbw.sh
