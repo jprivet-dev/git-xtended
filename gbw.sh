be576f060b826ad0d2256c1fae952e75eea3e9a7
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 10:19:53 +0100

    dev.refactor(gbw.sh): include "bash/gbw.bash_aliases.sh"

--- gbw.sh
+++ gbw.sh
@@ -17,8 +17,8 @@ echo " @info https://github.com/jprivet-dev/git-bash-workflow.git"
 echo
 
 source ~/git-bash-workflow/src/gbw.variables.sh
-
 source ~/git-bash-workflow/config/gbw.params.sh
+
 if [[ -f ~/git-bash-workflow/params.sh ]]; then
     source ~/git-bash-workflow/params.sh
 fi
@@ -29,6 +29,6 @@ source ~/git-bash-workflow/src/gbw.colors.sh
 source ~/git-bash-workflow/src/gbw.git.sh
 source ~/git-bash-workflow/src/gbw.prompt.sh
 
-source ~/git-bash-workflow/src/gbw.bash_aliases.sh
+source ~/git-bash-workflow/bash/gbw.bash_aliases.sh
 
 gbw_prompt_init
