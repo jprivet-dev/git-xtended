6701807a12f5c8be1d6ddf0b709ecd0b5dec1d96
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 15:21:43 +0100

    dev.fix(gbw.tests.fake.sh): typo > --porcelain(e)

--- src/tests/gbw.tests.fake.sh
+++ src/tests/gbw.tests.fake.sh
@@ -6,19 +6,17 @@ function gbw_tests_fake_git {
     local args="$*"
 
     case $args in
-        "status --porcelaine")
+        "status --porcelain")
             echo "M  changes-to-be-committed--modified"
             echo "A  changes-to-be-committed--added"
             echo "D  changes-to-be-committed--deleted"
             echo "R  changes-to-be-committed--renamed"
             echo "C  changes-to-be-committed--copied"
-
             echo " M changes-not-staged-for-commit--modified"
             echo " A changes-not-staged-for-commit--added"
             echo " D changes-not-staged-for-commit--deleted"
             echo " R changes-not-staged-for-commit--renamed"
             echo " C changes-not-staged-for-commit--copied"
-
             echo "?? untracked-files"
         ;;
         "branch")
