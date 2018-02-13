0de4f8afc7ecc3c155bab27258f3718d5b3a8b51
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 10:27:33 +0100

    chore.refactor(gbw.tests.xxx.sh): move all in "src/tests" folder

---
+++ src/tests/gbw.tests.init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function gbw_tests_find_all_func_test {
+    compgen -A function | grep test_
+}
+
+function gbw_tests_run_all {
+    gbw_tests_find_all_func_test | while read func; do
+        echo "==> $func"
+        $func
+        echo
+    done
+}
