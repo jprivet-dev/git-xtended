c836686c6e3a73e5aedb33472d9f2e9cfa24b18c
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 18:06:06 +0100

    dev(gbw.git.tests.sh): create test_gbw_git_get_status_changes_to_be_committed_count

--- tests/gbw.git.tests.sh
+++ tests/gbw.git.tests.sh
@@ -11,3 +11,8 @@ function test_gbw_git_get_changes_nb {
 function test_gbw_git_diff_current_branch_origin_dev {
     assert equals "$(gbw_git_diff_current_branch_origin_dev | wc -l)" "5" $LINENO
 }
+
+function test_gbw_git_get_status_changes_to_be_committed_count {
+    assert equals "$(gbw_git_get_status_changes_to_be_committed_count)" "5" $LINENO
+}
+
