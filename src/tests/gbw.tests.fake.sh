e86ab03ea8f4eb4dad0e33fa6624856cd2d7459d
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 14:57:26 +0100

    dev(gbw.tests.fake.sh): create base of gbw_tests_fake_git

--- src/tests/gbw.tests.fake.sh
+++ src/tests/gbw.tests.fake.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
 GBW_USER="user"
+
+function gbw_tests_fake_git {
+    local args="$*"
+    case $args in
+        "status --porcelaine")
+            echo "M  changes-to-be-committed--modified"
+            echo "A  changes-to-be-committed--added"
+            echo "D  changes-to-be-committed--deleted"
+            echo "R  changes-to-be-committed--renamed"
+            echo "C  changes-to-be-committed--copied"
+
+            echo " M changes-not-staged-for-commit--modified"
+            echo " A changes-not-staged-for-commit--added"
+            echo " D changes-not-staged-for-commit--deleted"
+            echo " R changes-not-staged-for-commit--renamed"
+            echo " C changes-not-staged-for-commit--copied"
+
+            echo "?? untracked-files"
+        ;;
+    esac
+}
