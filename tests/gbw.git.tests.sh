1013e450a9068f8b2fb13460b500b6fae2230eb8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 15:21:59 +0100

    dev(gbw.git.tests.sh): create test_gbw_git_get_changes_nb

--- tests/gbw.git.tests.sh
+++ tests/gbw.git.tests.sh
@@ -3,3 +3,7 @@
 function test_gbw_git_get_current_branch {
     assert equals "$(gbw_git_get_current_branch)" "current-branch" $LINENO
 }
+
+function test_gbw_git_get_changes_nb {
+    assert equals "$(gbw_git_get_changes_nb)" "11" $LINENO
+}
