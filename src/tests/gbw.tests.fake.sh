08c1a11396b8e8c585a03d1113daa4e79db7e7a4
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 18:03:12 +0100

    dev.style(gbw.tests.fake.sh): add blank line

--- src/tests/gbw.tests.fake.sh
+++ src/tests/gbw.tests.fake.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 GBW_USER="user"
+
 function gbw_tests_fake_git_status_porcelain {
     echo "M  changes-to-be-committed--modified"
     echo "A  changes-to-be-committed--added"
