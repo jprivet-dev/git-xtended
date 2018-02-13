2a41201bb1ed9855c845f1f4beadfb7cb80910da
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 14:57:06 +0100

    dev(gbw.params.sh): set LC_ALL

--- config/gbw.params.sh
+++ config/gbw.params.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 # Change local
-#export LC_ALL=C
+if [ -z ${LC_ALL+x} ]; then
+    echo "# LC_ALL is unset > export LC_ALL=C"
+    export LC_ALL=C
+else
+    echo "# LC_ALL is set to '$var'"
+fi
 
 GBW_BRANCH_MAIN_REMOTE_DEVELOP="origin/develop"
 GBW_BRANCH_MAIN_REMOTE_RELEASE="origin/release"
