c7024c03e124ff777af4b75f8b935b27fa7ecc73
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-13 16:50:06 +0100

    dev.refactor(gbw.tests.fake.sh): split gbw_tests_fake_git

--- src/tests/gbw.tests.fake.sh
+++ src/tests/gbw.tests.fake.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
 GBW_USER="user"
+function gbw_tests_fake_git_status_porcelain {
+    echo "M  changes-to-be-committed--modified"
+    echo "A  changes-to-be-committed--added"
+    echo "D  changes-to-be-committed--deleted"
+    echo "R  changes-to-be-committed--renamed"
+    echo "C  changes-to-be-committed--copied"
+    echo " M changes-not-staged-for-commit--modified"
+    echo " A changes-not-staged-for-commit--added"
+    echo " D changes-not-staged-for-commit--deleted"
+    echo " R changes-not-staged-for-commit--renamed"
+    echo " C changes-not-staged-for-commit--copied"
+    echo "?? untracked-files"
+}
+
+function gbw_tests_fake_git_branch {
+    echo "  fake-branch-1"
+    echo "  fake-branch-2"
+    echo "* current-branch"
+    echo "  develop"
+    echo "  master"
+}
 
 function gbw_tests_fake_git {
     local args="$*"
 
     case $args in
         "status --porcelain")
-            echo "M  changes-to-be-committed--modified"
-            echo "A  changes-to-be-committed--added"
-            echo "D  changes-to-be-committed--deleted"
-            echo "R  changes-to-be-committed--renamed"
-            echo "C  changes-to-be-committed--copied"
-            echo " M changes-not-staged-for-commit--modified"
-            echo " A changes-not-staged-for-commit--added"
-            echo " D changes-not-staged-for-commit--deleted"
-            echo " R changes-not-staged-for-commit--renamed"
-            echo " C changes-not-staged-for-commit--copied"
-            echo "?? untracked-files"
+            gbw_tests_fake_git_status_porcelain
         ;;
         "branch")
-            echo "  fake-branch-1"
-            echo "  fake-branch-2"
-            echo "* current-branch"
-            echo "  develop"
-            echo "  master"
+            gbw_tests_fake_git_branch
         ;;
     esac
 }
