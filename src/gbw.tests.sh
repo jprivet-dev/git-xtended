99ba8aad938b1595060ec114e79970a2d079bd6e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-14 17:15:09 +0100

    chore.refactor(test/): folder renaming

--- src/gbw.tests.sh
+++ src/gbw.tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-source ~/git-bash-workflow/src/tests/gbw.tests.main.sh
-source ~/git-bash-workflow/src/tests/gbw.tests.fake.sh
-source ~/git-bash-workflow/src/tests/gbw.tests.assert.sh
+source ~/git-bash-workflow/src/test/gbw.tests.main.sh
+source ~/git-bash-workflow/src/test/gbw.tests.fake.sh
+source ~/git-bash-workflow/src/test/gbw.tests.assert.sh
