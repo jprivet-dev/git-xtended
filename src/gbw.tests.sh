c270197a9e78f302b9ed906aa10fc26c08c34fd1
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-14 17:18:05 +0100

    chore.refactor(gbw.test.xxx.sh): files renaming

--- src/gbw.tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source ~/git-bash-workflow/src/test/gbw.tests.main.sh
-source ~/git-bash-workflow/src/test/gbw.tests.fake.sh
-source ~/git-bash-workflow/src/test/gbw.tests.assert.sh
