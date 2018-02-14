46bcdb4d238b43be31f2f7efa0d48efee536d9f2
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-14 15:46:55 +0100

    dev.refactor(gbw.prompt.tests.sh): vars renaming

--- tests/gbw.prompt.tests.sh
+++ tests/gbw.prompt.tests.sh
@@ -1,67 +1,67 @@
 #!/usr/bin/env bash
 
 function test_gbw_prompt_time {
-    assert equals "$(gbw_prompt_time)" "$TEST_GBW_PROMPT_TIME" $LINENO
+    assert equals "$(gbw_prompt_time)" "$_TEST_GBW_PROMPT_TIME" $LINENO
 }
 
 function test_gbw_prompt_user {
-    assert equals "$(gbw_prompt_user)" "$TEST_GBW_PROMPT_USER" $LINENO
+    assert equals "$(gbw_prompt_user)" "$_TEST_GBW_PROMPT_USER" $LINENO
 }
 
 function test_gbw_prompt_host {
-    assert equals "$(gbw_prompt_host)" "$TEST_GBW_PROMPT_HOST" $LINENO
+    assert equals "$(gbw_prompt_host)" "$_TEST_GBW_PROMPT_HOST" $LINENO
 }
 
 function test_gbw_prompt_userhost {
-    assert equals "$(gbw_prompt_userhost)" "$TEST_GBW_PROMPT_USERHOST_ESCAPE_AT" $LINENO
+    assert equals "$(gbw_prompt_userhost)" "$_TEST_GBW_PROMPT_USERHOST_ESCAPE_AT" $LINENO
 }
 
 function test_gbw_prompt_dir {
-    assert equals "$(gbw_prompt_dir)" "$TEST_GBW_PROMPT_DIR" $LINENO
+    assert equals "$(gbw_prompt_dir)" "$_TEST_GBW_PROMPT_DIR" $LINENO
 }
 
 function test_gbw_prompt_branch {
-    assert equals "$(gbw_prompt_branch)" "$TEST_GBW_PROMPT_BRANCH" $LINENO
+    assert equals "$(gbw_prompt_branch)" "$_TEST_GBW_PROMPT_BRANCH" $LINENO
 }
 
 function test_gbw_prompt_changes_count {
-    assert equals "$(gbw_prompt_changes_count)" "$TEST_GBW_PROMPT_COUNT" $LINENO
+    assert equals "$(gbw_prompt_changes_count)" "$_TEST_GBW_PROMPT_COUNT" $LINENO
 }
 
 function test_gbw_prompt_status_to_be_commited {
-    assert equals "$(gbw_prompt_status_to_be_commited)" "$TEST_GBW_PROMPT_STATUS_C" $LINENO
+    assert equals "$(gbw_prompt_status_to_be_commited)" "$_TEST_GBW_PROMPT_STATUS_C" $LINENO
 }
 
 function test_gbw_prompt_status_not_staged {
-    assert equals "$(gbw_prompt_status_not_staged)" "$TEST_GBW_PROMPT_STATUS_S" $LINENO
+    assert equals "$(gbw_prompt_status_not_staged)" "$_TEST_GBW_PROMPT_STATUS_S" $LINENO
 }
 
 function test_gbw_prompt_status_untracked {
-    assert equals "$(gbw_prompt_status_untracked)" "$TEST_GBW_PROMPT_STATUS_U" $LINENO
+    assert equals "$(gbw_prompt_status_untracked)" "$_TEST_GBW_PROMPT_STATUS_U" $LINENO
 }
 
 function test_gbw_prompt_status {
-    assert equals "$(gbw_prompt_status)" "$TEST_GBW_PROMPT_STATUS" $LINENO
+    assert equals "$(gbw_prompt_status)" "$_TEST_GBW_PROMPT_STATUS" $LINENO
 }
 
 function test_gbw_prompt_behind {
     local restore="$GBW_GIT_REMOTE_BRANCH_REF"
 
     GBW_GIT_REMOTE_BRANCH_REF="$_GBW_FAKE_REMOTE_BRANCH_REF_NOK"
-    assert equals "$(gbw_prompt_behind)" "$TEST_GBW_PROMPT_BEHIND_NOK" $LINENO
+    assert equals "$(gbw_prompt_behind)" "$_TEST_GBW_PROMPT_BEHIND_NOK" $LINENO
 
     GBW_GIT_REMOTE_BRANCH_REF="$restore"
-    assert equals "$(gbw_prompt_behind)" "$TEST_GBW_PROMPT_BEHIND" $LINENO
+    assert equals "$(gbw_prompt_behind)" "$_TEST_GBW_PROMPT_BEHIND" $LINENO
 }
 
 function test_gbw_prompt_ahead {
     local restore="$GBW_GIT_REMOTE_BRANCH_REF"
 
     GBW_GIT_REMOTE_BRANCH_REF="$_GBW_FAKE_REMOTE_BRANCH_REF_NOK"
-    assert equals "$(gbw_prompt_ahead)" "$TEST_GBW_PROMPT_AHEAD_NOK" $LINENO
+    assert equals "$(gbw_prompt_ahead)" "$_TEST_GBW_PROMPT_AHEAD_NOK" $LINENO
 
     GBW_GIT_REMOTE_BRANCH_REF="$restore"
-    assert equals "$(gbw_prompt_ahead)" "$TEST_GBW_PROMPT_AHEAD" $LINENO
+    assert equals "$(gbw_prompt_ahead)" "$_TEST_GBW_PROMPT_AHEAD" $LINENO
 }
 
 # TODO
@@ -70,9 +70,9 @@ function test_gbw_prompt_ahead {
 #}
 
 function test_gbw_prompt_ps1_part1 {
-    assert equals "$(gbw_prompt_ps1_part1)" "$TEST_GBW_PROMPT_PS1_PART1" $LINENO
+    assert equals "$(gbw_prompt_ps1_part1)" "$_TEST_GBW_PROMPT_PS1_PART1" $LINENO
 }
 
 function test_gbw_prompt_ps1_part2 {
-    assert equals "$(gbw_prompt_ps1_part2)" "$TEST_GBW_PROMPT_PS1_PART2" $LINENO
+    assert equals "$(gbw_prompt_ps1_part2)" "$_TEST_GBW_PROMPT_PS1_PART2" $LINENO
 }
