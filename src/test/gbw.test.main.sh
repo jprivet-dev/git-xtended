a9b8c33bd31ae5de7356ea4979af6ee4973c47c8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-02-14 17:48:57 +0100

    dev.refactor(gbw.test.main.sh): gbw_test_xxx > functions renaming

--- src/test/gbw.test.main.sh
+++ src/test/gbw.test.main.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-gbw_tests_count_tests=0
-gbw_tests_count_assertions=0
-gbw_tests_count_failures=0
+gbw_test_count_tests=0
+gbw_test_count_assertions=0
+gbw_test_count_failures=0
 
-function gbw_tests_count_tests_increment {
-    ((gbw_tests_count_tests++))
+function gbw_test_count_tests_increment {
+    ((gbw_test_count_tests++))
 }
 
-function gbw_tests_count_assertions_increment {
-    ((gbw_tests_count_assertions++))
+function gbw_test_count_assertions_increment {
+    ((gbw_test_count_assertions++))
 }
 
-function gbw_tests_count_failures_increment {
-    ((gbw_tests_count_failures++))
+function gbw_test_count_failures_increment {
+    ((gbw_test_count_failures++))
 }
 
-function gbw_tests_print_results {
+function gbw_test_print_results {
     local prefix
 
     local color_ok="$C_LIGHT_GREEN"
@@ -27,7 +27,7 @@ function gbw_tests_print_results {
     local assertion_plurial
     local failure_plurial
 
-    if [[ $gbw_tests_count_failures > 0 ]]; then
+    if [[ $gbw_test_count_failures > 0 ]]; then
         prefix="FAILURES !"
         color="$color_nok"
     else
@@ -35,27 +35,27 @@ function gbw_tests_print_results {
         color="$color_ok"
     fi
 
-    [[ $gbw_tests_count_tests > 1 ]] && test_plurial="tests" || test_plurial="test"
-    [[ $gbw_tests_count_assertions > 1 ]] && assertion_plurial="assertions" || assertion_plurial="assertion"
-    [[ $gbw_tests_count_failures > 1 ]] && failure_plurial="failures" || failure_plurial="failure"
+    [[ $gbw_test_count_tests > 1 ]] && test_plurial="tests" || test_plurial="test"
+    [[ $gbw_test_count_assertions > 1 ]] && assertion_plurial="assertions" || assertion_plurial="assertion"
+    [[ $gbw_test_count_failures > 1 ]] && failure_plurial="failures" || failure_plurial="failure"
 
     echo
-    echo -e "$color$prefix ($gbw_tests_count_tests $test_plurial, $gbw_tests_count_assertions $assertion_plurial, $gbw_tests_count_failures $failure_plurial)$F_RESET"
+    echo -e "$color$prefix ($gbw_test_count_tests $test_plurial, $gbw_test_count_assertions $assertion_plurial, $gbw_test_count_failures $failure_plurial)$F_RESET"
 }
 
-function gbw_tests_find_all_func_test {
+function gbw_test_find_all_func_test {
     compgen -A function | grep ^test_
 }
 
-function gbw_tests_run_all {
-    func_test_list=($(gbw_tests_find_all_func_test))
+function gbw_test_run_all {
+    func_test_list=($(gbw_test_find_all_func_test))
 
     for func in "${func_test_list[@]}"
     do
         echo "-- $func"
         $func
-        gbw_tests_count_tests_increment
+        gbw_test_count_tests_increment
     done
 
-    gbw_tests_print_results
+    gbw_test_print_results
 }
