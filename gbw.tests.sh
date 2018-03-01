234477ff9aa46b978ef4d5a9620d51a6386e96f9
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-01 22:55:43 +0100

    chore.refactor(bash.aliases.sh): file renaming

--- gbw.tests.sh
+++ gbw.tests.sh
@@ -4,7 +4,7 @@
 # START TEST MODE #
 ###################
 
-GBW_TEST_MODE=1
+GBW_ENV_TEST=1
 
 echo "#############################"
 echo "# Git Bash Workflow TESTING #"
@@ -23,7 +23,7 @@ source ~/git-bash-workflow/tests/utils.tests.sh
 gbw_test_run_all
 
 unalias git
-unset GBW_TEST_MODE
+unset GBW_ENV_TEST
 
 ####################
 # FINISH TEST MODE #
