e195f348e4060d996aa5c5051919e6ade7eb6d5e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-02 11:39:40 +0100

    dev(utils.tests.sh): test_gbw_implode > improve test

--- tests/utils.tests.sh
+++ tests/utils.tests.sh
@@ -4,12 +4,21 @@ function test_gbw_implode {
     local _RED="\e[31m"
     local _RESET="\e[0m"
 
-    assert equals "$(gbw_implode " " "con" "ca" "te" "na" "te")"                        "con ca te na te" $LINENO
-    assert equals "$(gbw_implode "  " "con" "ca" "te" "na" "te")"                       "con  ca  te  na  te" $LINENO
-    assert equals "$(gbw_implode "-" "con" "ca" "te" "na" "te")"                        "con-ca-te-na-te" $LINENO
-    assert equals "$(gbw_implode "-"   "con"  "ca"  "te"      "na"  "te")"              "con-ca-te-na-te" $LINENO
-    assert equals "$(gbw_implode "-"   "con"  "ca"  "te"   ""   "na" "" "te")"          "con-ca-te-na-te" $LINENO
-    assert equals "$(gbw_implode "-"   "con"  "ca"  "te"   " "   "na"  "   "   "te")"   "con-ca-te-na-te" $LINENO
+    assert equals "$(gbw_implode "-" "con" "ca" "te" "na" "te")"                                "con-ca-te-na-te" $LINENO
+    assert equals "$(gbw_implode "-"   "con"  "ca"  "te"      "na"  "te")"                      "con-ca-te-na-te" $LINENO
+    assert equals "$(gbw_implode "-"   "con"  "ca"  "te"   ""   "na" "" "te")"                  "con-ca-te-na-te" $LINENO
+    assert equals "$(gbw_implode "-"   "con"  "ca"  "te"   " "   "na"  "   "   "te")"           "con-ca-te-na-te" $LINENO
+
+    assert equals "$(gbw_implode " " "con" "ca" "te" "na" "te")"                                "con ca te na te" $LINENO
+    assert equals "$(gbw_implode " "   "con"  "ca"  "te"      "na"  "te")"                      "con ca te na te" $LINENO
+    assert equals "$(gbw_implode " "   "con"  "ca"  "te"   ""   "na" "" "te")"                  "con ca te na te" $LINENO
+    assert equals "$(gbw_implode " "   "con"  "ca"  "te"   ""   ""   "na" "" "te")"             "con ca te na te" $LINENO
+    assert equals "$(gbw_implode " "   "con"  "ca"  "te"   " "   " "   "na"  "   "   "te")"     "con ca te na te" $LINENO
+
+    assert equals "$(gbw_implode "  " "con" "ca" "te" "na" "te")"                               "con  ca  te  na  te" $LINENO
+    assert equals "$(gbw_implode "  "   "con"  "ca"  "te"      "na"  "te")"                     "con  ca  te  na  te" $LINENO
+    assert equals "$(gbw_implode "  "   "con"  "ca"  "te"   ""   ""   "na" "" "te")"            "con  ca  te  na  te" $LINENO
+    assert equals "$(gbw_implode "  "   "con"  "ca"  "te"   " "   " "   "na"  "   "   "te")"    "con  ca  te  na  te" $LINENO
 
     assert equals "$(gbw_implode " - " "con" "ca" "te" "na" "te")"                      "con - ca - te - na - te" $LINENO
     assert equals "$(gbw_implode " \e[31m-\e[0m " "con" "ca" "te" "na" "te")"           "con \e[31m-\e[0m ca \e[31m-\e[0m te \e[31m-\e[0m na \e[31m-\e[0m te" $LINENO
