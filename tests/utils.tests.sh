d30013bed372331f83ddb4f8d62ca462d043a9ac
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-02 10:58:17 +0100

    dev(utils.tests.sh): test_gbw_implode > improve test

--- tests/utils.tests.sh
+++ tests/utils.tests.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 function test_gbw_implode {
+    local _RED="\e[31m"
+    local _RESET="\e[0m"
+
     assert equals "$(gbw_implode " " "con" "ca" "te" "na" "te")" "con ca te na te" $LINENO
     assert equals "$(gbw_implode "  " "con" "ca" "te" "na" "te")" "con  ca  te  na  te" $LINENO
     assert equals "$(gbw_implode "-" "con" "ca" "te" "na" "te")" "con-ca-te-na-te" $LINENO
     assert equals "$(gbw_implode " - " "con" "ca" "te" "na" "te")" "con - ca - te - na - te" $LINENO
+    assert equals "$(gbw_implode " \e[31m-\e[0m " "con" "ca" "te" "na" "te")" "con \e[31m-\e[0m ca \e[31m-\e[0m te \e[31m-\e[0m na \e[31m-\e[0m te" $LINENO
+    assert equals "$(gbw_implode " $_RED-$_RESET " "con" "ca" "te" "na" "te")" "con \e[31m-\e[0m ca \e[31m-\e[0m te \e[31m-\e[0m na \e[31m-\e[0m te" $LINENO
 
     local email_part_1="address"
     local email_part_2="email.com"
