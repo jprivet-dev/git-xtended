e7937aa1ca9a84c7254e8f61440d70ebc997615f
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-05 22:21:17 +0100

    dev(test.main.sh): create gbw_test_check_func_with_test and adjust colors of titles

--- src/test/test.main.sh
+++ src/test/test.main.sh
@@ -35,9 +35,9 @@ function gbw_test_print_results {
         color="$color_ok"
     fi
 
-    [[ $gbw_test_count_tests > 1 ]] && test_plurial="tests" || test_plurial="test"
-    [[ $gbw_test_count_assertions > 1 ]] && assertion_plurial="assertions" || assertion_plurial="assertion"
-    [[ $gbw_test_count_failures > 1 ]] && failure_plurial="failures" || failure_plurial="failure"
+    [[ $gbw_test_count_tests > 1 ]]         && test_plurial="tests"             || test_plurial="test"
+    [[ $gbw_test_count_assertions > 1 ]]    && assertion_plurial="assertions"   || assertion_plurial="assertion"
+    [[ $gbw_test_count_failures > 1 ]]      && failure_plurial="failures"       || failure_plurial="failure"
 
     echo
     echo -e "$color$prefix ($gbw_test_count_tests $test_plurial, $gbw_test_count_assertions $assertion_plurial, $gbw_test_count_failures $failure_plurial)$F_RESET"
@@ -47,8 +47,16 @@ function gbw_test_find_all_func_test {
     compgen -A function | grep ^test_
 }
 
+function gbw_test_find_all_func_gbw {
+    compgen -A function | grep ^gbw_
+}
+
 function gbw_test_run_all {
-    func_test_list=($(gbw_test_find_all_func_test))
+    local func_test_list=($(gbw_test_find_all_func_test))
+
+    echo
+    echo -e "${C_LIGHT_YELLOW}# Testing launch${F_RESET}";
+    echo
 
     for func in "${func_test_list[@]}"
     do
@@ -58,4 +66,21 @@ function gbw_test_run_all {
     done
 
     gbw_test_print_results
+}
+
+function gbw_test_check_func_with_test {
+    local func_gbw_list=($(gbw_test_find_all_func_gbw))
+
+    echo
+    echo -e "${C_LIGHT_YELLOW}# Functions with test${F_RESET}";
+    echo
+
+    for func in "${func_gbw_list[@]}"
+    do
+        func_test_version="test_$func"
+
+        type $func_test_version &>/dev/null \
+            && echo -e "$C_LIGHT_GREEN[x]  $func$F_RESET" \
+            || echo -e "$C_LIGHT_BLUE[ ]  $func$F_RESET"
+    done
 }
