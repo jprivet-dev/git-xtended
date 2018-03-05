05731576e72ead90530d68b7d988b90495960472
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-05 22:38:43 +0100

    dev(test.fake.sh): gbw_test_fake_git > avoid response for '--version'

--- src/test/test.fake.sh
+++ src/test/test.fake.sh
@@ -81,6 +81,9 @@ function gbw_test_fake_git {
         "rev-parse --git-dir")
             gbw_test_fake_git_rev_parse_git_dir
         ;;
+        "--version")
+            return
+        ;;
         *)
             "[ ERROR !!! No Git fake command for '$args' ]"
         ;;
