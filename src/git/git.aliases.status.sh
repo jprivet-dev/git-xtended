24ce95bd767e84e89070f0ac34820d789b94d12c
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-08 15:13:05 +0100

    dev.refactor(git.aliases.status.sh): use _status_index instead of INDEX

--- src/git/git.aliases.status.sh
+++ src/git/git.aliases.status.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+_status_index=0
 while IFS= read -r line; do
-    INDEX=$(expr $INDEX + 1)
-    echo -e "$INDEX) $line"
+    _status_index=$(expr $_status_index + 1)
+    echo -e "$_status_index) $line"
 done < <(git status --porcelain)
