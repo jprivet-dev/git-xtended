b59e55b1a2eca45fb50e90835622a0676b1ae6f5
Jean-Philippe Rivet <jprivet@groupe361.com>
2018-03-08 16:34:02 +0100

    dev.refactor(git.aliases.commit.sh): reorganisation of if conditions

--- src/git/git.aliases.commit.sh
+++ src/git/git.aliases.commit.sh
@@ -4,18 +4,25 @@ _commit_choose_indexes=$@
 
 if [ "$_commit_choose_indexes" == "" ]; then
     echo "> COMMIT ERROR: Choose index !"
-elif [ "$_commit_choose_indexes" == "all" ] || [ "$_commit_choose_indexes" == "." ]; then
+    exit 1
+fi
+
+if [ "$_commit_choose_indexes" == "all" ] || [ "$_commit_choose_indexes" == "." ]; then
+    echo
     git add .
     git status -s -u
     git commit -m ""
-else
-    _commit_index=0
-    git status -s | cut -c4- | while read line; do
-        _commit_index=$(expr $_commit_index + 1)
-        if [ "$_commit_index" == "$_commit_choose_indexes" ]; then
-            git add $line
-            git status -s -u
-            git commit -m ""
-        fi
-    done
+    exit 1
 fi
+
+_commit_index=0
+
+git status -s | cut -c4- | while read line; do
+    _commit_index=$(expr $_commit_index + 1)
+    if [ "$_commit_index" == "$_commit_choose_indexes" ]; then
+        echo
+        git add $line
+        git status -s -u
+        git commit -m ""
+    fi
+done
