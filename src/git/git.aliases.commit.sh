bc86ae2e611617bda339a2208d0d8c21ef95a795
Jean-Philippe Rivet <jprivet@groupe361.com>
2018-03-08 16:23:56 +0100

    dev(git.aliases.commit.sh): show status

--- src/git/git.aliases.commit.sh
+++ src/git/git.aliases.commit.sh
@@ -6,7 +6,7 @@ if [ "$_commit_choose_indexes" == "" ]; then
     echo "> COMMIT ERROR: Choose index !"
 elif [ "$_commit_choose_indexes" == "all" ]; then
     git add .
-    echo "> ADD: all"
+    git status -s -u
     git commit -m ""
 else
     _commit_index=0
@@ -14,7 +14,7 @@ else
         _commit_index=$(expr $_commit_index + 1)
         if [ "$_commit_index" == "$_commit_choose_indexes" ]; then
             git add $line
-            echo "> ADD: $line"
+            git status -s -u
             git commit -m ""
         fi
     done
