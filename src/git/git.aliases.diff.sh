034399470cc824c9267e52b9a7fbffb778908e22
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-09 11:08:19 +0100

    dev(git.aliases.diff.sh): create base of file

---
+++ src/git/git.aliases.diff.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+_index=$1
+
+if [ "$_index" == "" ]; then
+    git diff
+    exit 1
+fi
+
+_current_index=0
+
+git status -s | cut -c4- | while read line; do
+    _current_index=$(expr $_current_index + 1)
+    if [ "$_current_index" == "$_index" ]; then
+        git diff $line
+    fi
+done
