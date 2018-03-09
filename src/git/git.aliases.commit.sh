f8652de269b15f0f3c6f4f52a9ff033bc33532fe
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-09 12:04:01 +0100

    dev.clean(git.aliases.commit.sh): remove _active_git_status

--- src/git/git.aliases.commit.sh
+++ src/git/git.aliases.commit.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 _commit_choose_indexes=$@
-_active_git_status=0
 _split="--------------------------------------------------"
 
 if [ "$_commit_choose_indexes" == "all" ] || [ "$_commit_choose_indexes" == "." ]; then
@@ -13,7 +12,6 @@ if [ "$_commit_choose_indexes" == "all" ] || [ "$_commit_choose_indexes" == "." 
 fi
 
 if [ "$_commit_choose_indexes" == "" ]; then
-    _active_git_status=1
     _commit_choose_indexes=1
 fi
 
@@ -22,11 +20,6 @@ _commit_index=0
 git status -s | cut -c4- | while read line; do
     _commit_index=$(expr $_commit_index + 1)
     if [ "$_commit_index" == "$_commit_choose_indexes" ]; then
-        if [ "$_active_git_status" == 1 ]; then
-            echo "$_split"
-            git diff $line
-        fi
-
         git add $line
 
         echo
