e4eefea8a459c2520d9f0d8c6f82d9444e3088b8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-09 15:26:37 +0100

    dev.refactor(git.aliases.status.sh): rename vars

--- src/git/git.aliases.status.sh
+++ src/git/git.aliases.status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-_status_index=0
+_index=0
 while IFS= read -r line; do
-    _status_index=$(expr $_status_index + 1)
-    echo -e "$_status_index) $line"
+    _index=$(expr $_index + 1)
+    echo -e "$_index) $line"
 done < <(git status --porcelain)
