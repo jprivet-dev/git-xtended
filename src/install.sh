4da36ec985a43fc1e43c7e57697499f648e8772a
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 15:28:54 +0100

    dev.refactor(install.sh): params renaming & titles formatting

--- src/install.sh
+++ src/install.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-####################################
-#              PROMPT              #
-####################################
+# ----------------------------------
+# PROMPT
+# ----------------------------------
 
 gbw_prompt_init_state="$GBW_PARAMS_OFF_C"
 
-if [ "$GBW_PARAMS_PROMPT_INIT_ACTIVE" == 1 ]; then
+if [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == 1 ]; then
     gbw_prompt_init
     gbw_prompt_init_state="$GBW_PARAMS_ON_C"
 fi
 
 echo -e "| Prompt with Git information                   | $gbw_prompt_init_state        |"
 
-####################################
-#           GIT ALIASES            #
-####################################
+# ----------------------------------
+# GIT ALIASES
+# ----------------------------------
 
-if [ "$GBW_PARAMS_GIT_ALIASES_INIT_ACTIVE" == 1 ]; then
+if [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == 1 ]; then
     gbw_git_aliases_enable
     gbw_git_aliases_init_state="$GBW_PARAMS_ON_C"
 else
@@ -27,11 +27,11 @@ fi
 
 echo -e "| Git aliases                                   | $gbw_git_aliases_init_state        |"
 
-####################################
-#            GIT HOOKS             #
-####################################
+# ----------------------------------
+# GIT HOOKS
+# ----------------------------------
 
-if [ "$GBW_PARAMS_GIT_HOOKS_INIT_ACTIVE" == 1 ]; then
+if [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == 1 ]; then
     gbw_git_config_hooks_enable
     gbw_git_hooks_init_state="$GBW_PARAMS_ON_C"
 else
@@ -41,8 +41,8 @@ fi
 
 echo -e "| Git hooks                                     | $gbw_git_hooks_init_state        |"
 
-####################################
-#          GIT WORKFLOW            #
-####################################
+# ----------------------------------
+# GIT WORKFLOW
+# ----------------------------------
 
 echo -e "| Agile Git workflow commands                   | (wip)      |"
