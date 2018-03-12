277305572e3cb7c3f22dc871fe88ef13477912f3
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 22:15:32 +0100

    dev.refactor(params.git.sh): create file & insert here git params

---
+++ config/params/params.git.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+GBW_PARAMS_GIT_ALIAS_KEY_PROMPT_ACTIVE="gbw.prompt"
+
+GBW_PARAMS_GIT_ALIAS_DIFF_FILE="$GBW_PARAMS_ROOT/src/git/alias/git.alias.diff.sh"
+GBW_PARAMS_GIT_ALIAS_COMMIT_FILE="$GBW_PARAMS_ROOT/src/git/alias/git.alias.commit.sh"
+GBW_PARAMS_GIT_ALIAS_STATUS_FILE="$GBW_PARAMS_ROOT/src/git/alias/git.alias.status.sh"
+
+GBW_PARAMS_GIT_HOOKS_DIR="$GBW_PARAMS_ROOT/git/hooks/"
+
+GBW_PARAMS_GIT_REMOTE_BRANCH_REF=""
+
+
