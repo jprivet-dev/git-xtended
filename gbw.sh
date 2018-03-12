9471b031e16ad76a6ac955cbeccc088c6ac10e1d
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 15:18:35 +0100

    dev.clean(gbw.sh): move parts activation in install.sh

--- gbw.sh
+++ gbw.sh
@@ -20,52 +20,11 @@ source ~/git-bash-workflow/src/bash.sh
 echo
 echo -e "+------------------------------------------------------------+"
 echo -e "| Git Bash Workflow                                    $GBW_PARAMS_VERSION |"
-echo -e "+-----------------------------------------------+------------+"
+echo -e "+------------------------------------------------------------+"
 
-####################################
-#              PROMPT              #
-####################################
+source ~/git-bash-workflow/src/install.sh
 
-gbw_prompt_init_state="$GBW_PARAMS_OFF_C"
-
-if [ "$GBW_PARAMS_PROMPT_INIT_ACTIVE" == 1 ]; then
-    gbw_prompt_init
-    gbw_prompt_init_state="$GBW_PARAMS_ON_C"
-fi
-
-echo -e "| Prompt with Git information                   | $gbw_prompt_init_state        |"
-
-####################################
-#           GIT ALIASES            #
-####################################
-
-if [ "$GBW_PARAMS_GIT_ALIASES_INIT_ACTIVE" == 1 ]; then
-    gbw_git_aliases_enable
-    gbw_git_aliases_init_state="$GBW_PARAMS_ON_C"
-else
-    gbw_git_aliases_disable
-    gbw_git_aliases_init_state="$GBW_PARAMS_OFF_C"
-fi
-
-echo -e "| Git aliases                                   | $gbw_git_aliases_init_state        |"
-
-####################################
-#            GIT HOOKS             #
-####################################
-
-if [ "$GBW_PARAMS_GIT_HOOKS_INIT_ACTIVE" == 1 ]; then
-    gbw_git_config_hooks_enable
-    gbw_git_hooks_init_state="$GBW_PARAMS_ON_C"
-else
-    gbw_git_config_hooks_disable
-    gbw_git_hooks_init_state="$GBW_PARAMS_OFF_C"
-fi
-
-echo -e "| Git hooks                                     | $gbw_git_hooks_init_state        |"
-
-echo -e "| Agile Git workflow commands                   | (wip)      |"
-
-echo -e "+-----------------------------------------------+------------+"
+echo -e "+------------------------------------------------------------+"
 echo -e "| @info https://github.com/jprivet-dev/git-bash-workflow.git |"
 echo -e "+------------------------------------------------------------+"
 echo
