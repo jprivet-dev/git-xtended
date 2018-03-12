db632f37a8e9393170b4ec8757f569acc891db5f
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 16:34:35 +0100

    dev(params.sh): import params.install.sh

--- config/params.sh
+++ config/params.sh
@@ -3,4 +3,5 @@
 source ~/git-bash-workflow/config/params/params.version.sh
 source ~/git-bash-workflow/config/params/params.colors.sh
 source ~/git-bash-workflow/config/params/params.main.sh
+source ~/git-bash-workflow/config/params/params.install.sh
 
