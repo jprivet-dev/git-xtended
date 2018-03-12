eb98583d5c78fff451af0d8dd067aa36929ba24c
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 15:34:44 +0100

    dev(bash.aliases.sh): create gbw_bash_aliases_set_all

--- src/bash/bash.aliases.sh
+++ src/bash/bash.aliases.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-alias g="git"
+function gbw_bash_aliases_set_all {
+    alias g="git"
 
-if [[ -x "$(git --version)" ]]; then
-    # active git autocompletion for 'g' alias
-    complete -o default -o nospace -F _git g
-fi
+    if [[ -x "$(git --version)" ]]; then
+        # active git autocompletion for 'g' alias
+        complete -o default -o nospace -F _git g
+    fi
 
-alias reload="source ~/.bashrc"
-alias reloadp="source ~/.bash_profile"
+    alias reload="source ~/.bashrc"
+    alias reloadp="source ~/.bash_profile"
 
-alias c="clear"
+    alias c="clear"
 
-alias ls="ls -alF --color=auto"
-alias ls.="ls -d .* --color=auto"
+    alias ls="ls -alF --color=auto"
+    alias ls.="ls -d .* --color=auto"
 
-alias t="source ~/.bashrc; source gbw.tests.sh"
+    alias t="source ~/.bashrc; source gbw.tests.sh"
+}
+
