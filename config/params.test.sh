b70c9feb61204fbbd693b6d991dc40037983394e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 10:42:30 +0100

    conf.refactor(params.test.sh): file renaming

--- config/params.test.sh
+++ config/params.test.sh
@@ -2,3 +2,4 @@
 
 source ~/git-bash-workflow/config/params/test/params.test.main.sh
 source ~/git-bash-workflow/config/params/test/params.test.prompt.sh
+source ~/git-bash-workflow/config/params/test/params.test.overload.sh
