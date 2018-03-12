d43b87a3735e2800af715755e4c776de1dffec3a
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 10:50:06 +0100

    dev.refactor(gbw.sh): import bash.sh instead of bash.aliases.sh

--- gbw.sh
+++ gbw.sh
@@ -20,7 +20,7 @@ source ~/git-bash-workflow/src/commands.sh
 source ~/git-bash-workflow/src/colors.sh
 source ~/git-bash-workflow/src/git.sh
 source ~/git-bash-workflow/src/prompt.sh
-source ~/git-bash-workflow/src/bash.aliases.sh
+source ~/git-bash-workflow/src/bash.sh
 
 ####################################
 #              PROMPT              #
