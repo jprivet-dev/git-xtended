d0a554120097acbfb96fb2a971433dc5284afd8e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 22:16:56 +0100

    conf.clean(params.main.sh): move git params in params.git.sh file

--- config/params/params.main.sh
+++ config/params/params.main.sh
@@ -12,12 +12,6 @@ GBW_PARAMS_OFF_C="${C_LIGHT_RED}off${F_RESET}"
 GBW_PARAMS_USER_PS1="\u"
 GBW_PARAMS_ROOT="~/git-bash-workflow"
 
-GBW_PARAMS_GIT_ALIAS_DIFF_FILE="$GBW_PARAMS_ROOT/src/git/alias/git.alias.diff.sh"
-GBW_PARAMS_GIT_ALIAS_COMMIT_FILE="$GBW_PARAMS_ROOT/src/git/alias/git.alias.commit.sh"
-GBW_PARAMS_GIT_ALIAS_STATUS_FILE="$GBW_PARAMS_ROOT/src/git/alias/git.alias.status.sh"
-
-GBW_PARAMS_GIT_HOOKS_DIR="$GBW_PARAMS_ROOT/git/hooks/"
-
 # Change local
 #if [ -z ${LC_ALL+x} ]; then
 #    echo "# LC_ALL is unset > export LC_ALL=C"
@@ -26,5 +20,4 @@ GBW_PARAMS_GIT_HOOKS_DIR="$GBW_PARAMS_ROOT/git/hooks/"
 #    echo "# LC_ALL is set to '$var'"
 #fi
 
-GBW_PARAMS_GIT_REMOTE_BRANCH_REF=""
 
