94476c99489f1c0dd6459d13db1efb1b85d87188
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-12 21:41:01 +0100

    dev(git.config.sh): create file & import git.config.xxx.sh

---
+++ src/git/git.config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source ~/git-bash-workflow/src/git/config/git.config.accessors.sh
+source ~/git-bash-workflow/src/git/config/git.config.aliases.sh
+source ~/git-bash-workflow/src/git/config/git.config.hooks.sh
