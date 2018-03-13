7a88742a594e1ab840fcc77d839dd14ab48cefde
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-13 14:40:57 +0100

    dev(git.config.workflow.sh): create base of file

---
+++ src/git/config/git.config.workflow.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# TODO: complete action
+function gbw_git_config_workflow_enable {
+    local nope="on"
+}
+
+# TODO: complete action
+function gbw_git_config_workflow_disable {
+    local nope="off"
+}
