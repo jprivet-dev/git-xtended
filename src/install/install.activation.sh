48c4231f4cac85561ed8e8eedb799047c587fe62
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-13 15:25:42 +0100

    dev.refactor(install.activation.sh): gbw_install_activation_show_status > simplify func with gbw_install_activation_on_off_color

--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -10,33 +10,18 @@ function gbw_install_activation {
     [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]   && gbw_bash_aliases_on          || gbw_bash_aliases_off
 }
 
+function gbw_install_activation_on_off_color {
+    [ "$1" == "$GBW_PARAMS_ON" ] && echo "$GBW_PARAMS_ON_C" || echo "$GBW_PARAMS_OFF_C"
+}
+
 function gbw_install_activation_show_status {
     gbw_install_choice_params_set_all_from_git_config
 
-    local gbw_prompt_on_off="$GBW_PARAMS_OFF_C"
-    if [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_prompt_on_off="$GBW_PARAMS_ON_C"
-    fi
-
-    local gbw_git_aliases_on_off="$GBW_PARAMS_OFF_C"
-    if [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_git_aliases_on_off="$GBW_PARAMS_ON_C"
-    fi
-
-    local gbw_git_hooks_on_off="$GBW_PARAMS_OFF_C"
-    if [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_git_hooks_on_off="$GBW_PARAMS_ON_C"
-    fi
-
-    local gbw_workflow_on_off="$GBW_PARAMS_OFF_C"
-    if [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_workflow_on_off="$GBW_PARAMS_ON_C"
-    fi
-
-    local gbw_bash_aliases_on_off="$GBW_PARAMS_OFF_C"
-    if [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_bash_aliases_on_off="$GBW_PARAMS_ON_C"
-    fi
+    local gbw_prompt_on_off="$(gbw_install_activation_on_off_color $GBW_PARAMS_INSTALL_PROMPT_ACTIVE)"
+    local gbw_git_aliases_on_off="$(gbw_install_activation_on_off_color $GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE)"
+    local gbw_git_hooks_on_off="$(gbw_install_activation_on_off_color $GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE)"
+    local gbw_workflow_on_off="$(gbw_install_activation_on_off_color $GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE)"
+    local gbw_bash_aliases_on_off="$(gbw_install_activation_on_off_color $GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE)"
 
     echo
     echo -e "+------------------------------------------------------------+"
