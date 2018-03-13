e99835c8b8d0d8a58101bdd3e6023c48827eba6d
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-13 13:38:31 +0100

    dev(git.config.accessors.sh): gwb_git_config_get > add default value argument in option

--- src/git/config/git.config.accessors.sh
+++ src/git/config/git.config.accessors.sh
@@ -2,7 +2,17 @@
 
 # TODO: create test
 function gwb_git_config_get {
-    git config --global $1
+    local key=$1
+    local default=$2
+
+    local value="$(git config --global $key)"
+
+    if [ "$value" == "" ]; then
+        echo "$default"
+        return
+    fi
+
+    echo "$value"
 }
 
 # TODO: create test
