90a3b2ad2ea6faa96bf90316ce420909fc031114
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-13 14:42:05 +0100

    dev.clean(install.activation.sh): gbw_install_activation > remove unless code

--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -3,60 +3,20 @@
 function gbw_install_activation {
     gbw_install_choice_params_set_all_from_git_config
 
-    # ----------------------------------
-    # PROMPT
-    # ----------------------------------
-
-    if [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
-        gbw_prompt_init
-    fi
-
-    # ----------------------------------
-    # GIT ALIASES
-    # ----------------------------------
-
-    if [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
-        gbw_git_aliases_enable
-    else
-        gbw_git_aliases_disable
-    fi
-
-    # ----------------------------------
-    # GIT HOOKS
-    # ----------------------------------
-
-    if [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
-        gbw_git_config_hooks_enable
-    else
-        gbw_git_config_hooks_disable
-    fi
-
-    # ----------------------------------
-    # GIT WORKFLOW
-    # ----------------------------------
-
-
-    if [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
-        #gbw_git_config_workflow_enable
-        nope=1
-    else
-        #gbw_git_config_workflow_disable
-        nope=12
-    fi
-
-    # ----------------------------------
-    # BASH ALIASES
-    # ----------------------------------
-
-    if [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
-        gbw_bash_aliases_enable
-    else
-        gbw_bash_aliases_disable
-    fi
+    [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]         && gbw_prompt_init
+    [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]    && gbw_git_aliases_enable           || gbw_git_aliases_disable
+    [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]      && gbw_git_config_hooks_enable      || gbw_git_config_hooks_disable
+    [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]       && gbw_git_config_workflow_enable   || gbw_git_config_workflow_disable
+    [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]   && gbw_bash_aliases_enable          || gbw_bash_aliases_disable
 }
 
 function gbw_install_activation_show_status {
     gbw_install_choice_params_set_all_from_git_config
+
+    echo
+    echo -e "+------------------------------------------------------------+"
+    echo -e "| $GBW_PARAMS_TITLE                                    $GBW_PARAMS_VERSION |"
+    echo -e "+------------------------------------------------------------+"
 
     # ----------------------------------
     # PROMPT
@@ -125,4 +85,9 @@ function gbw_install_activation_show_status {
     fi
 
     echo -e "| Bash aliases                                  | $gbw_bash_aliases_init_state        |"
+
+    echo -e "+------------------------------------------------------------+"
+    echo -e "| @info https://github.com/jprivet-dev/git-bash-workflow.git |"
+    echo -e "+------------------------------------------------------------+"
+    echo
 }
