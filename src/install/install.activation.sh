72207a24d1527f302722d3c395f3c786d9817387
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-13 15:08:06 +0100

    dev.refactor(install.activation.sh): use GBW_PARAMS_ON instead

--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -3,11 +3,11 @@
 function gbw_install_activation {
     gbw_install_choice_params_set_all_from_git_config
 
-    [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]         && gbw_prompt_on
-    [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]    && gbw_git_aliases_on           || gbw_git_aliases_off
-    [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]      && gbw_git_config_hooks_on      || gbw_git_config_hooks_off
-    [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]       && gbw_git_config_workflow_on   || gbw_git_config_workflow_off
-    [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]   && gbw_bash_aliases_on          || gbw_bash_aliases_off
+    [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ON" ]         && gbw_prompt_on
+    [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]    && gbw_git_aliases_on           || gbw_git_aliases_off
+    [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ON" ]      && gbw_git_config_hooks_on      || gbw_git_config_hooks_off
+    [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ON" ]       && gbw_git_config_workflow_on   || gbw_git_config_workflow_off
+    [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]   && gbw_bash_aliases_on          || gbw_bash_aliases_off
 }
 
 function gbw_install_activation_show_status {
@@ -24,7 +24,7 @@ function gbw_install_activation_show_status {
 
     local gbw_prompt_init_state="$GBW_PARAMS_OFF_C"
 
-    if [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
+    if [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ON" ]; then
         gbw_prompt_init_state="$GBW_PARAMS_ON_C"
     fi
 
@@ -36,7 +36,7 @@ function gbw_install_activation_show_status {
 
     local gbw_git_aliases_init_state
 
-    if [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
+    if [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
         gbw_git_aliases_init_state="$GBW_PARAMS_ON_C"
     else
         gbw_git_aliases_init_state="$GBW_PARAMS_OFF_C"
@@ -50,7 +50,7 @@ function gbw_install_activation_show_status {
 
     local gbw_git_hooks_init_state
 
-    if [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
+    if [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ON" ]; then
         gbw_git_hooks_init_state="$GBW_PARAMS_ON_C"
     else
         gbw_git_hooks_init_state="$GBW_PARAMS_OFF_C"
@@ -64,7 +64,7 @@ function gbw_install_activation_show_status {
 
     local gbw_workflow_init_state
 
-    if [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
+    if [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ON" ]; then
         gbw_workflow_init_state="$GBW_PARAMS_ON_C"
     else
         gbw_workflow_init_state="$GBW_PARAMS_OFF_C"
@@ -78,7 +78,7 @@ function gbw_install_activation_show_status {
 
     local gbw_bash_aliases_init_state
 
-    if [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ACTIVE" ]; then
+    if [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
         gbw_bash_aliases_init_state="$GBW_PARAMS_ON_C"
     else
         gbw_bash_aliases_init_state="$GBW_PARAMS_OFF_C"
