70026b334a67b953140ff4e8c7a457026181e26d
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-13 15:19:24 +0100

    dev.clean(install.activation.sh): gbw_install_activation_show_status > simplify func

--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -13,70 +13,41 @@ function gbw_install_activation {
 function gbw_install_activation_show_status {
     gbw_install_choice_params_set_all_from_git_config
 
+    local gbw_prompt_on_off="$GBW_PARAMS_OFF_C"
+    if [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ON" ]; then
+        gbw_prompt_on_off="$GBW_PARAMS_ON_C"
+    fi
+
+    local gbw_git_aliases_on_off="$GBW_PARAMS_OFF_C"
+    if [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
+        gbw_git_aliases_on_off="$GBW_PARAMS_ON_C"
+    fi
+
+    local gbw_git_hooks_on_off="$GBW_PARAMS_OFF_C"
+    if [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ON" ]; then
+        gbw_git_hooks_on_off="$GBW_PARAMS_ON_C"
+    fi
+
+    local gbw_workflow_on_off="$GBW_PARAMS_OFF_C"
+    if [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ON" ]; then
+        gbw_workflow_on_off="$GBW_PARAMS_ON_C"
+    fi
+
+    local gbw_bash_aliases_on_off="$GBW_PARAMS_OFF_C"
+    if [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
+        gbw_bash_aliases_on_off="$GBW_PARAMS_ON_C"
+    fi
+
     echo
     echo -e "+------------------------------------------------------------+"
     echo -e "| $GBW_PARAMS_TITLE                                    $GBW_PARAMS_VERSION |"
     echo -e "+------------------------------------------------------------+"
 
-    # ----------------------------------
-    # PROMPT
-    # ----------------------------------
-
-    local gbw_prompt_init_state="$GBW_PARAMS_OFF_C"
-
-    if [ "$GBW_PARAMS_INSTALL_PROMPT_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_prompt_init_state="$GBW_PARAMS_ON_C"
-    fi
-
-    echo -e "| $GBW_PARAMS_LABEL_PROMPT                   | $gbw_prompt_init_state        |"
-
-    # ----------------------------------
-    # GIT ALIASES
-    # ----------------------------------
-
-    local gbw_git_aliases_init_state="$GBW_PARAMS_OFF_C"
-
-    if [ "$GBW_PARAMS_INSTALL_GIT_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_git_aliases_init_state="$GBW_PARAMS_ON_C"
-    fi
-
-    echo -e "| Git aliases                                   | $gbw_git_aliases_init_state        |"
-
-    # ----------------------------------
-    # GIT HOOKS
-    # ----------------------------------
-
-    local gbw_git_hooks_init_state="$GBW_PARAMS_OFF_C"
-
-    if [ "$GBW_PARAMS_INSTALL_GIT_HOOKS_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_git_hooks_init_state="$GBW_PARAMS_ON_C"
-    fi
-
-    echo -e "| Git hooks                                     | $gbw_git_hooks_init_state        |"
-
-    # ----------------------------------
-    # GIT WORKFLOW
-    # ----------------------------------
-
-    local gbw_workflow_init_state="$GBW_PARAMS_OFF_C"
-
-    if [ "$GBW_PARAMS_INSTALL_WORKFLOW_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_workflow_init_state="$GBW_PARAMS_ON_C"
-    fi
-
-    echo -e "| $GBW_PARAMS_LABEL_WORKFLOW               | $gbw_workflow_init_state (wip)  |"
-
-    # ----------------------------------
-    # BASH ALIASES
-    # ----------------------------------
-
-    local gbw_bash_aliases_init_state="$GBW_PARAMS_OFF_C"
-
-    if [ "$GBW_PARAMS_INSTALL_BASH_ALIASES_ACTIVE" == "$GBW_PARAMS_ON" ]; then
-        gbw_bash_aliases_init_state="$GBW_PARAMS_ON_C"
-    fi
-
-    echo -e "| Bash aliases                                  | $gbw_bash_aliases_init_state        |"
+    echo -e "| $GBW_PARAMS_LABEL_PROMPT                   | $gbw_prompt_on_off        |"
+    echo -e "| Git aliases                                   | $gbw_git_aliases_on_off        |"
+    echo -e "| Git hooks                                     | $gbw_git_hooks_on_off        |"
+    echo -e "| $GBW_PARAMS_LABEL_WORKFLOW               | $gbw_workflow_on_off (wip)  |"
+    echo -e "| Bash aliases                                  | $gbw_bash_aliases_on_off        |"
 
     echo -e "+------------------------------------------------------------+"
     echo -e "| @info https://github.com/jprivet-dev/git-bash-workflow.git |"
