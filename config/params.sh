0a74f7428a280c07d765dc6e128f9abe09dee1aa
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-14 10:43:16 +0100

    dev.fix(params.sh): load params.main.sh earlier

--- config/params.sh
+++ config/params.sh
@@ -2,7 +2,8 @@
 
 source ~/git-bash-workflow/config/params/params.version.sh
 source ~/git-bash-workflow/config/params/params.colors.sh
+source ~/git-bash-workflow/config/params/params.main.sh
+
 source ~/git-bash-workflow/config/params/params.git.sh
 source ~/git-bash-workflow/config/params/params.install.sh
-source ~/git-bash-workflow/config/params/params.main.sh
 
