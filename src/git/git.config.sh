f55439f94361f8a76fe4a99a05080f0e8def0fec
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-15 22:15:24 +0100

    dev.refactor(git.config.sh): import git.config.workflow.help.sh

--- src/git/git.config.sh
+++ src/git/git.config.sh
@@ -4,3 +4,4 @@ source ~/git-bash-workflow/src/git/config/git.config.accessors.sh
 source ~/git-bash-workflow/src/git/config/git.config.aliases.sh
 source ~/git-bash-workflow/src/git/config/git.config.hooks.sh
 source ~/git-bash-workflow/src/git/config/git.config.workflow.sh
+source ~/git-bash-workflow/src/git/config/git.config.workflow.help.sh
