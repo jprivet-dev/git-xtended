9fc27e82eaec6081c861a8d7d0227fbac32a3259
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-15 22:15:06 +0100

    dev.clean(git.config.workflow.sh): remove gbw_git_config_workflow_help

--- src/git/config/git.config.workflow.sh
+++ src/git/config/git.config.workflow.sh
@@ -9,9 +9,3 @@ function gbw_git_config_workflow_on {
 function gbw_git_config_workflow_off {
     local nope="off"
 }
-
-function gbw_git_config_workflow_help {
-    echo -e "${C_WHITE}${GBW_PARAMS_LABEL_WORKFLOW}${F_RESET} :"
-    echo "${GBW_PARAMS_TAB}XX ... WIP"
-    echo
-}
