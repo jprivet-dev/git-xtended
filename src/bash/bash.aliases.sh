ae53530a52392d0bc16b7571d863681a05747792
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-15 22:25:02 +0100

    dev.clean(bash.aliases.sh): remove gbw_bash_aliases_help

--- src/bash/bash.aliases.sh
+++ src/bash/bash.aliases.sh
@@ -29,9 +29,3 @@ function gbw_bash_aliases_off {
     unalias ls.
 }
 
-function gbw_bash_aliases_help {
-    echo -e "${C_WHITE}${GBW_PARAMS_LABEL_BASH_ALIASES}${F_RESET} :"
-    echo "${GBW_PARAMS_TAB}XX ... WIP"
-    echo
-}
-
