42ce5bd7ddb75400e706d5c44521227aa64171ef
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-15 09:42:36 +0100

    dev.refactor(prompt.sh): gbw_prompt_help > use "_colors" serie

--- src/prompt.sh
+++ src/prompt.sh
@@ -163,13 +163,13 @@ function gbw_prompt_ahead_colors {
 
 # @test
 function gbw_prompt_help {
-    local branch="$(gbw_prompt_branch)"
-    local count="$(gbw_prompt_changes_count)"
-    local status_c="$(gbw_prompt_status_to_be_commited)"
-    local status_s="$(gbw_prompt_status_not_staged)"
-    local status_u="$(gbw_prompt_status_untracked)"
-    local behind="$(gbw_prompt_behind)"
-    local ahead="$(gbw_prompt_ahead)"
+    local branch=$(gbw_prompt_branch_colors "branch")
+    local count=$(gbw_prompt_changes_count_colors "9")
+    local status_u=$(gbw_prompt_status_untracked_colors "2")
+    local status_s=$(gbw_prompt_status_not_staged_colors "3")
+    local status_c=$(gbw_prompt_status_to_be_commited_colors "2" "0" "1")
+    local behind=$(gbw_prompt_behind_colors "3")
+    local ahead=$(gbw_prompt_ahead_colors "42")
 
     echo "$GBW_PARAMS_LABEL_PROMPT :"
     gbw_prompt_help_line_dots "$branch"      "Current branch"
