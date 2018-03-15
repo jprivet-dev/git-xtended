8bf5ef65ef20e0f981b1fe8acb62478333d4c8ed
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-15 22:22:19 +0100

    dev.refactor(git.config.sh): import git.config.aliases.help.sh

--- src/git/git.config.sh
+++ src/git/git.config.sh
@@ -2,6 +2,7 @@
 
 source ~/git-bash-workflow/src/git/config/git.config.accessors.sh
 source ~/git-bash-workflow/src/git/config/git.config.aliases.sh
+source ~/git-bash-workflow/src/git/config/git.config.aliases.help.sh
 source ~/git-bash-workflow/src/git/config/git.config.hooks.sh
 source ~/git-bash-workflow/src/git/config/git.config.hooks.help.sh
 source ~/git-bash-workflow/src/git/config/git.config.workflow.sh
