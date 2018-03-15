82c9c5b06346963e5536f51813c3263ed670564b
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-15 22:18:20 +0100

    dev.refactor(git.config.sh): import git.config.hooks.help.sh

--- src/git/git.config.sh
+++ src/git/git.config.sh
@@ -3,5 +3,6 @@
 source ~/git-bash-workflow/src/git/config/git.config.accessors.sh
 source ~/git-bash-workflow/src/git/config/git.config.aliases.sh
 source ~/git-bash-workflow/src/git/config/git.config.hooks.sh
+source ~/git-bash-workflow/src/git/config/git.config.hooks.help.sh
 source ~/git-bash-workflow/src/git/config/git.config.workflow.sh
 source ~/git-bash-workflow/src/git/config/git.config.workflow.help.sh
