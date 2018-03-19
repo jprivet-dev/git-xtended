52b8d3a8bb9757ce40d5df4e5d51c03649a6961e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-19 21:56:54 +0100

    dev.refactor(params.test.sh): import params.test.colors.sh

--- config/params.test.sh
+++ config/params.test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source ~/git-bash-workflow/config/params/test/params.test.colors.sh
 source ~/git-bash-workflow/config/params/test/params.test.main.sh
 source ~/git-bash-workflow/config/params/test/params.test.prompt.sh
 source ~/git-bash-workflow/config/params/test/params.test.overload.sh
