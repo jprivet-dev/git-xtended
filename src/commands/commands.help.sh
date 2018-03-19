2f6bd95fb07088f0f8dd0719710869898938ff85
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-19 22:35:46 +0100

    dev.refactor(all files): use ${xxx} structure

--- src/commands/commands.help.sh
+++ src/commands/commands.help.sh
@@ -2,11 +2,11 @@
 
 function gbw_command_help {
     echo -e "${C_WHITE}${GBW_PARAMS_LABEL_GBW_COMMANDS}${F_RESET} :"
-    gbw_command_help_command_line "help" "" ............ "Display help information about $GBW_PARAMS_TITLE_SHORT"
-    gbw_command_help_command_line "install" "" ......... "Select and install $GBW_PARAMS_TITLE_SHORT elements"
-    gbw_command_help_command_line "reload" "" .......... "Reload $GBW_PARAMS_TITLE_SHORT"
-    gbw_command_help_command_line "status" "" .......... "Show status of $GBW_PARAMS_TITLE_SHORT elements"
-    gbw_command_help_command_line "test"  "[<func>]" ... "Run one or all $GBW_PARAMS_TITLE_SHORT tests"
+    gbw_command_help_command_line "help" "" ............ "Display help information about ${GBW_PARAMS_TITLE_SHORT}"
+    gbw_command_help_command_line "install" "" ......... "Select and install ${GBW_PARAMS_TITLE_SHORT} elements"
+    gbw_command_help_command_line "reload" "" .......... "Reload ${GBW_PARAMS_TITLE_SHORT}"
+    gbw_command_help_command_line "status" "" .......... "Show status of ${GBW_PARAMS_TITLE_SHORT} elements"
+    gbw_command_help_command_line "test"  "[<func>]" ... "Run one or all ${GBW_PARAMS_TITLE_SHORT} tests"
     echo
 }
 
