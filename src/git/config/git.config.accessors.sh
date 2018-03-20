6240153e9df748815cdb8f6635ccad647ddb83ef
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-20 14:58:49 +0100

    dev(git.config.accessors.sh): create gbw_git_config_set_global_alias

--- src/git/config/git.config.accessors.sh
+++ src/git/config/git.config.accessors.sh
@@ -20,6 +20,10 @@ function gwb_git_config_set {
     git config --global $1 $2
 }
 
+function gbw_git_config_set_global_alias {
+    $(gbw_git_config_set_global_alias_cmd $1 "$2")
+}
+
 function gbw_git_config_set_global_alias_cmd {
     echo "git config --global alias.$1 \"$2\""
 }
