298eb019029db26811b702308c8d047cdeca99ab
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-21 13:45:56 +0100

    dev(test.params.sh): echo info

--- src/test/test.params.sh
+++ src/test/test.params.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+echo "==> GBW: import src/test/test.params.sh"
+
 gbw_test_count_tests=0
 gbw_test_count_assertions=0
 gbw_test_count_failures=0
 gbw_test_assert_pipeline_message=()
 
-gbw_test_fake_git_last_command=""
+gbw_test_fake_git_last_args=""
