545c8f3150405ef26c3d03ab5886a591370cc7b0
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-21 23:43:05 +0100

    dev(test.tests.sh): update test_gbw_test_fake_git

--- tests/test.tests.sh
+++ tests/test.tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function test_gbw_test_fake_git {
-    local no_fake_command_args="no fake command"
-    assert equals "$(git ${no_fake_command_args})" "[ ERROR !!! No Git fake command for '${no_fake_command_args}' ]" $LINENO
-    assert equals "$(gbw_test_fake_git_last_args_get)" "${no_fake_command_args}" $LINENO
+    local git_no_fake_command="git no fake command"
+    assert equals "$(git no fake command)" "[ ERROR !!! No Git fake command for '${git_no_fake_command}' ]" $LINENO
+    assert last-git-command-is "${git_no_fake_command}" $LINENO
 }
