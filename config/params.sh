9a70296c4f1b676e7f011158a1b23ff186fa16a1
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-21 13:45:21 +0100

    dev(params.sh): echo info

--- config/params.sh
+++ config/params.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "==> GBW: import config/params.sh"
+
 source ~/git-bash-workflow/config/params/params.version.sh
 source ~/git-bash-workflow/config/params/params.colors.sh
 source ~/git-bash-workflow/config/params/params.main.sh
