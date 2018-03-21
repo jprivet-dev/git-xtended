e68f9e11c218569b943ea3d9fa07a29e3e89ab92
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-21 13:21:33 +0100

    dev(test.tests.sh): create base of file

---
+++ tests/test.tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+function test_gbw_test_fake_git {
+    local no_fake_command="no fake command"
+    assert equals "$(git ${no_fake_command})" "[ ERROR !!! No Git fake command for '${no_fake_command}' ]" $LINENO
+}
