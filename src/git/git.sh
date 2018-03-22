9fea4d847690a63764631f3b204f219c8899fee4
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 09:22:55 +0100

    chore.refactor(git): move all files in git folder

--- src/git/git.sh
+++ src/git/git.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-source ~/git-bash-workflow/src/git/git.config.sh
-source ~/git-bash-workflow/src/git/git.commands.sh
+source ~/git-bash-workflow/src/git/config/git.config.sh
+source ~/git-bash-workflow/src/git/commands/git.commands.sh
