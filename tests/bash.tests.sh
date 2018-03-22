6d58f187064dcba5ff83d7b8f0e37e55d4c3ac2d
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 14:30:24 +0100

    dev(bash.tests.sh): test_gbw_bash_aliases_help > use assert last-line

--- tests/bash.tests.sh
+++ tests/bash.tests.sh
@@ -3,7 +3,5 @@
 function test_gbw_bash_aliases_help {
     assert first-line   "`gbw_bash_aliases_help`"   "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
     assert second-line  "`gbw_bash_aliases_help`"   "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
-
-    local last_line="$(gbw_bash_aliases_help | tail -n1)"
-    assert equals "${last_line}" "" $LINENO
+    assert last-line    "`gbw_bash_aliases_help`"   "" $LINENO
 }
