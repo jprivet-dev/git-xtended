98a5bd7a4213c2f3e49e59239ab3a3bb906be8a1
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 15:32:29 +0100

    dev.refactor(git.config.workflow.help.sh): gbw_git_config_workflow_help > use gbw_print

--- src/git/config/git.config.workflow.help.sh
+++ src/git/config/git.config.workflow.help.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function gbw_git_config_workflow_help {
-    echo -e "${C_WHITE}${GBW_PARAMS_LABEL_WORKFLOW}${F_RESET} :"
-    echo "${GBW_PARAMS_TAB}XX ... WIP"
-    echo
+    gbw_print_colors "${C_WHITE}${GBW_PARAMS_LABEL_WORKFLOW}${F_RESET} :"
+    gbw_print "${GBW_PARAMS_TAB}XX ... WIP"
+    gbw_print
 }
