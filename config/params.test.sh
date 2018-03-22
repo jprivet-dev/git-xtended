bccd0aeeafee793df6fc503f5d61753bc537fe2a
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 08:58:28 +0100

    chore.refactor(all project): move params file in default folder

--- config/params.test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "==> GBW: import config/params.test.sh"
-
-source ~/git-bash-workflow/config/params/test/params.test.colors.sh
-source ~/git-bash-workflow/config/params/test/params.test.main.sh
-source ~/git-bash-workflow/config/params/test/params.test.prompt.sh
-source ~/git-bash-workflow/config/params/test/params.test.overload.sh
---
+++ params/test/params.test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "==> GBW: import params/test/params.test.sh"
+
+source ~/git-bash-workflow/params/test/params.test.colors.sh
+source ~/git-bash-workflow/params/test/params.test.main.sh
+source ~/git-bash-workflow/params/test/params.test.prompt.sh
+source ~/git-bash-workflow/params/test/params.test.overload.sh
