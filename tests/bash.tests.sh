ca5e914e2a19577fbbb1c3793c3d5005af7b3377
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 13:57:19 +0100

    dev(bash.tests.sh): test_gbw_bash_aliases_help > insert TEST_GBW_PARAMS_TAB

--- tests/bash.tests.sh
+++ tests/bash.tests.sh
@@ -5,7 +5,7 @@ function test_gbw_bash_aliases_help {
     assert equals "${first_line}" "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
 
     local second_line="$(gbw_bash_aliases_help | head -n2 | tail -n1)"
-    assert equals "${second_line}" "XX ... WIP" $LINENO
+    assert equals "${second_line}" "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
 
     local last_line="$(gbw_bash_aliases_help | tail -n1)"
     assert equals "${last_line}" "" $LINENO
