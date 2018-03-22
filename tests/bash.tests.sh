ab92ee68493f8c197d2d5423857af9283533a107
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 12:41:33 +0100

    dev(bash.tests.sh): update test_gbw_bash_aliases_help

--- tests/bash.tests.sh
+++ tests/bash.tests.sh
@@ -2,11 +2,11 @@
 
 function test_gbw_bash_aliases_help {
     local first_line="$(gbw_bash_aliases_help | head -n1)"
-    assert equals "${first_line}" "jjj" $LINENO
+    assert equals "${first_line}" "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
 
     local second_line="$(gbw_bash_aliases_help | head -n2 | tail -n1)"
-    assert equals "${second_line}" "jjj" $LINENO
+    assert equals "${second_line}" "XX ... WIP" $LINENO
 
     local last_line="$(gbw_bash_aliases_help | tail -n1)"
-    assert equals "${last_line}" "jjj" $LINENO
+    assert equals "${last_line}" "" $LINENO
 }
