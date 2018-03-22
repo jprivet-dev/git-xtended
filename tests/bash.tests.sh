2ef42a12791b5276fbd18c19b4313024fa0709d7
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 15:18:39 +0100

    dev.clean(bash.tests.sh): simplify test_gbw_bash_aliases_help

--- tests/bash.tests.sh
+++ tests/bash.tests.sh
@@ -2,6 +2,5 @@
 
 function test_gbw_bash_aliases_help {
     assert first-line   "`gbw_bash_aliases_help`"   "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
-    assert second-line  "`gbw_bash_aliases_help`"   "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
     assert last-line    "`gbw_bash_aliases_help`"   "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
 }
