b61ac7c961f06b05517c45efdfb53afe6eeca514
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 15:17:18 +0100

    dev(commands.tests.sh): update test_gbw_command_help

--- tests/commands.tests.sh
+++ tests/commands.tests.sh
@@ -2,6 +2,6 @@
 
 function test_gbw_command_help {
     assert first-line   "`gbw_command_help`"   "${TEST_C_WHITE}(6) GBW commands${TEST_F_RESET} :" $LINENO
-    assert second-line  "`gbw_command_help`"   "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
-    assert last-line    "`gbw_command_help`"   "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
+    assert second-line  "`gbw_command_help`"   "${TEST_GBW_PARAMS_TAB}gbw ${TEST_C_LIGHT_YELLOW}help${TEST_F_RESET} ${TEST_C_DARK_GRAY}............${TEST_F_RESET} Display help information about GBW" $LINENO
+    assert last-line    "`gbw_command_help`"   "${TEST_GBW_PARAMS_TAB}gbw ${TEST_C_LIGHT_YELLOW}test${TEST_F_RESET} [<func>] ${TEST_C_DARK_GRAY}...${TEST_F_RESET} Run one or all GBW tests" $LINENO
 }
