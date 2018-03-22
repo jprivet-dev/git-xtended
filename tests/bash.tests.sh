87f8fbf6c4ab8e141d6f5ec03a33252da8e378e4
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 14:20:00 +0100

    dev(bash.tests.sh): test_gbw_bash_aliases_help > use first-line

--- tests/bash.tests.sh
+++ tests/bash.tests.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 function test_gbw_bash_aliases_help {
-    local first_line="$(gbw_bash_aliases_help | head -n1)"
-    assert equals "${first_line}" "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
+    assert first-line "`gbw_bash_aliases_help`" "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
 
     local second_line="$(gbw_bash_aliases_help | head -n2 | tail -n1)"
     assert equals "${second_line}" "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
