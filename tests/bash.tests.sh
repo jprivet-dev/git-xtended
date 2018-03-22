ef2ae78f7350b5bf88301d8ada8981e46cbec32b
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-03-22 14:26:33 +0100

    dev(bash.tests.sh): test_gbw_bash_aliases_help > use assert second-line

--- tests/bash.tests.sh
+++ tests/bash.tests.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 function test_gbw_bash_aliases_help {
-    assert first-line "`gbw_bash_aliases_help`" "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
-
-    local second_line="$(gbw_bash_aliases_help | head -n2 | tail -n1)"
-    assert equals "${second_line}" "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
+    assert first-line   "`gbw_bash_aliases_help`"   "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
+    assert second-line  "`gbw_bash_aliases_help`"   "${TEST_GBW_PARAMS_TAB}XX ... WIP" $LINENO
 
     local last_line="$(gbw_bash_aliases_help | tail -n1)"
     assert equals "${last_line}" "" $LINENO
