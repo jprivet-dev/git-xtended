1812c9b4d30c571a46858a34f32424c0932de510
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-04 16:20:27 +0200

    dev(commands.tests.sh): update test_gbw_command_help

--- tests/commands.tests.sh
+++ tests/commands.tests.sh
@@ -2,7 +2,7 @@
 
 function test_gbw_command_help {
     local count_lines="`gbw_command_help | wc -l`"
-    assert equals "${count_lines}" "7" $LINENO
+    assert equals "${count_lines}" "8" $LINENO
 
     assert first-line   "`gbw_command_help`"   "${TEST_C_WHITE}(6) GBW commands${TEST_F_RESET} :" $LINENO
     assert last-line    "`gbw_command_help`"   "${TEST_GBW_PARAMS_TAB}gbw ${TEST_C_LIGHT_YELLOW}test${TEST_F_RESET} [<func>] ${TEST_C_DARK_GRAY}...${TEST_F_RESET} Run one or all GBW tests" $LINENO
