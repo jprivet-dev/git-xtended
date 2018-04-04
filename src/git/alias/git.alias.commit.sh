74f868df26d464ce559a28e7431f2e149d8f37b0
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-04 14:57:52 +0200

    dev.refactor(git.alias.commit.sh): replace $(...)

--- src/git/alias/git.alias.commit.sh
+++ src/git/alias/git.alias.commit.sh
@@ -18,7 +18,7 @@ fi
 _current_index=0
 
 git status -s | cut -c4- | while read line; do
-    _current_index=$(expr $_current_index + 1)
+    _current_index=`expr $_current_index + 1`
     if [ "${_current_index}" == "${_indexes}" ]; then
         git add $line
 
