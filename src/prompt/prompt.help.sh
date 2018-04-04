5b86d356b2ff8c754873f2974ff8d1d2a0b29b1e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-04 10:06:18 +0200

    dev.debug(prompt.help.sh): adjust gbw_prompt_help & gbw_prompt_help_line

--- src/prompt/prompt.help.sh
+++ src/prompt/prompt.help.sh
@@ -11,21 +11,17 @@ function gbw_prompt_help {
     local ahead=$(gbw_prompt_ahead_colors "42")
 
     echo -e "${C_WHITE}${GBW_PARAMS_LABEL_PROMPT}${F_RESET} :"
-    gbw_prompt_help_line_colors "${branch}"      .....           "Current branch"
-    gbw_prompt_help_line_colors "${count}"       ...........     "All elements from git status"
-    gbw_prompt_help_line_colors "${status_u}"    ...........     "Untracked files"
-    gbw_prompt_help_line_colors "${status_s}"    ...........     "Changes not staged for commit"
-    gbw_prompt_help_line_colors "${status_c}"    ...             "Changes to be committed"
-    gbw_prompt_help_line_colors "${behind}"      ...........     "Commits behind remote branch reference"
-    gbw_prompt_help_line_colors "${ahead}"       ..........      "Commits ahead remote branch reference"
+    gbw_prompt_help_line "${branch}"      .....           "Current branch"
+    gbw_prompt_help_line "${count}"       ...........     "All elements from git status"
+    gbw_prompt_help_line "${status_u}"    ...........     "Untracked files"
+    gbw_prompt_help_line "${status_s}"    ...........     "Changes not staged for commit"
+    gbw_prompt_help_line "${status_c}"    ...             "Changes to be committed"
+    gbw_prompt_help_line "${behind}"      ...........     "Commits behind remote branch reference"
+    gbw_prompt_help_line "${ahead}"       ..........      "Commits ahead remote branch reference"
     echo
 }
 
-function gbw_prompt_help_line_colors {
-    echo -e "$(gbw_prompt_help_line $*)"
-}
-
 function gbw_prompt_help_line {
-    echo "${GBW_PARAMS_TAB}$1 ${C_DARK_GRAY}$2${F_RESET} $3"
+    gbw_print_colors "${GBW_PARAMS_TAB}$1 ${C_DARK_GRAY}$2${F_RESET} $3"
 }
 
