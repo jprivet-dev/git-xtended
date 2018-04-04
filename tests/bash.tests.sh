05e0ccf22b8c0e46f82ab2885d10c79bf5dccbc6
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-04 13:53:53 +0200

    dev.refactor(bash.tests.sh): replace $(...)

--- tests/bash.tests.sh
+++ tests/bash.tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function test_gbw_bash_aliases_help {
-    local count_lines="$(gbw_bash_aliases_help | wc -l)"
+    local count_lines="`gbw_bash_aliases_help | wc -l`"
     assert equals "${count_lines}" "3" $LINENO
 
     assert first-line   "`gbw_bash_aliases_help`"   "${TEST_C_WHITE}(5) Bash aliases${TEST_F_RESET} :" $LINENO
