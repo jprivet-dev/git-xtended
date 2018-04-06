f6c0113aeb2c8c7281173defe43e7c6e94ff63ac
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-06 14:36:54 +0200

    dev(install.sh): include install.config.sh

--- src/install/install.sh
+++ src/install/install.sh
@@ -3,4 +3,5 @@
 source ~/git-bash-workflow/src/install/install.params.sh
 source ~/git-bash-workflow/src/install/install.choice.sh
 source ~/git-bash-workflow/src/install/install.activation.sh
+source ~/git-bash-workflow/src/install/install.config.sh
 
