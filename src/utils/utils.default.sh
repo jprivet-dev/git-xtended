d9ed938527440958ed672b9c9eb819647f0305a6
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-06 12:29:16 +0200

    chore.refactor(utils.default.sh): split utils.sh

---
+++ src/utils/utils.default.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# @test
+# Example :
+# test_1="hello"
+# test_2="bonjour"
+# test_3=""
+# gbw_implode ', ' 'a' 'b' "$test_1" 3 "$test_2" 4 "$test_3" 5
+# a, b, hello, 3, bonjour, 4, 5
+function gbw_implode {
+    local glue="$1"
+    local current_glue=""
+    shift
+
+    local -a pieces="($*)"
+    local implode=""
+
+    for p in "${pieces[@]}"
+    do
+        p="`gbw_trim "${p}"`"
+
+        if [[ "${implode}" == "" ]]; then
+            [[ "${p}" != "" ]] && implode="${p}"
+        else
+            [[ "${p}" != "" ]] && implode="${implode}${glue}${p}"
+        fi
+    done
+
+    echo "${implode}"
+}
+
+function gbw_is_bash_interactive {
+    [[ $- == *i* ]] \
+        && echo "true" \
+        || echo "false"
+}
+
+# -bash: $'\r': command not found
+function gbw_remove_r {
+    local file=$1
+    sed -i 's/\r$//' $file
+}
+
+function gbw_trim {
+    local string=$1
+    echo "${string}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
+}
+
+function gbw_regex_remove_colors {
+    echo "$1" | sed -E 's/\\e\[[0-9]{1,3}m//g'
+}
+
+function gbw_line_generator {
+    local length=$1
+    local symbol=$2
+    local result
+
+    printf -v generator '%*s' "${length}"
+    result=${generator// /$symbol}
+
+    echo "${result}"
+}
