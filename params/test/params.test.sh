58162e7ea1562f3087638f34a6f86d2ff902a2b2
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-06 14:07:29 +0200

    chore.refactor(params.test.default.sh): rename file

--- params/test/params.test.sh
+++ params/test/params.test.sh
@@ -3,6 +3,6 @@
 echo "==> GBW: import params/test/params.test.sh"
 
 source ~/git-bash-workflow/params/test/params.test.colors.sh
-source ~/git-bash-workflow/params/test/params.test.main.sh
+source ~/git-bash-workflow/params/test/params.test.default.sh
 source ~/git-bash-workflow/params/test/params.test.prompt.sh
 source ~/git-bash-workflow/params/test/params.test.overload.sh
