bcdedf4d2440634dc30c72b9bec242f78f3dac40
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-06 15:17:19 +0200

    dev(install.sh): import  bash-aliases, git-aliases, git-hooks, prompt & workflow

--- src/install/install.sh
+++ src/install/install.sh
@@ -5,3 +5,9 @@ source ~/git-bash-workflow/src/install/install.choice.sh
 source ~/git-bash-workflow/src/install/install.activation.sh
 source ~/git-bash-workflow/src/install/install.config.sh
 
+source ~/git-bash-workflow/src/install/install.prompt.sh
+source ~/git-bash-workflow/src/install/install.git-aliases.sh
+source ~/git-bash-workflow/src/install/install.git-hooks.sh
+source ~/git-bash-workflow/src/install/install.workflow.sh
+source ~/git-bash-workflow/src/install/install.bash-aliases.sh
+
