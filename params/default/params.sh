85e34ecdfad839d05c289a867ffe40d6b07a0280
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-06 14:06:27 +0200

    chore.refactor(params.default.sh): rename file

--- params/default/params.sh
+++ params/default/params.sh
@@ -4,7 +4,7 @@ echo "==> GBW: import params/default/params.sh"
 
 source ~/git-bash-workflow/params/default/params.version.sh
 source ~/git-bash-workflow/params/default/params.colors.sh
-source ~/git-bash-workflow/params/default/params.main.sh
+source ~/git-bash-workflow/params/default/params.default.sh
 
 source ~/git-bash-workflow/params/default/params.git.sh
 source ~/git-bash-workflow/params/default/params.install.sh
