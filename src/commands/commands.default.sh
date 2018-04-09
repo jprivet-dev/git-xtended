963b4cf5b7baa3e72a63dab19aada3de9f1b8962
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-09 17:19:34 +0200

    dev.clean(commands.default.sh): clear gbw_command_githooks (to remove later)

--- src/commands/commands.default.sh
+++ src/commands/commands.default.sh
@@ -90,28 +90,7 @@ function gbw_command_config_git_aliases {
     fi
 }
 
+# TODO: to remove
 function gbw_command_githooks {
-    local current_dir="${PWD}"
-    local gbw_pcm="${GBW_PARAMS_ROOT}/git/hooks/prepare-commit-msg"
-    local current_dir_hooks="${current_dir}/.git/hooks"
-    local current_dir_hook_pcm="${current_dir_hooks}/prepare-commit-msg"
-
-    local pcm_symlink_command_create="$ ln -sf ${gbw_pcm} ${current_dir_hook_pcm}"
-    local pcm_symlink_command_create_ls="$ ls ${current_dir_hooks}"
-    local pcm_symlink_command_delete="$ rm ${current_dir_hook_pcm}"
-
-    echo -e "${C_LIGHT_GREEN}"
-    echo -e "################################"
-    echo -e "# ${GBW_PARAMS_TITLE} - Githooks #"
-    echo -e "################################"
-    echo -e "${F_RESET}"
-
-    echo "Active prepare-commit-msg :"
-    echo "${pcm_symlink_command_create}"
-    echo "${pcm_symlink_command_create_ls}"
-    echo
-
-    echo "Delete prepare-commit-msg :"
-    echo "${pcm_symlink_command_delete}"
-    echo
+    echo "Removed"
 }
