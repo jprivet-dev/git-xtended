def93d44838ec2d306a84e181c415e79c4c1656f
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-10 14:06:18 +0200

    dev(install.git-hooks.sh): udpate gbw_install_git_hooks_symlink_xxx

--- src/install/install.git-hooks.sh
+++ src/install/install.git-hooks.sh
@@ -22,11 +22,9 @@ function gbw_install_git_hooks_corehookspath_not_available {
 
 function gbw_install_git_hooks_symlink {
     local current_hooks_prepare_commit_msg_path="${PWD}/.git/hooks/prepare-commit-msg"
-    local command_create_symlink="ln -sf ${GBW_PARAMS_HOOKS_PREPARE_COMMIT_MSG_PATH} ${current_hooks_prepare_commit_msg_path}"
-
-    gbw_print_step "Create symlink instead ($ ${command_create_symlink})"
 
     if [[ "`gbw_symlink_exists "$current_hooks_prepare_commit_msg_path"`" == "${GBW_PARAMS_TRUE}" ]]; then
+        gbw_print_step "Symlink allready exists (${current_hooks_prepare_commit_msg_path})"
         gbw_install_git_hooks_symlink_remove "${current_hooks_prepare_commit_msg_path}"
         return
     fi
@@ -36,9 +34,10 @@ function gbw_install_git_hooks_symlink {
 
 function gbw_install_git_hooks_symlink_create {
     local current_hooks_prepare_commit_msg_path="${1}"
-    local pcm_symlink_command_delete="rm ${current_hooks_prepare_commit_msg_path}"
+    local command_create_symlink="ln -sf ${GBW_PARAMS_HOOKS_PREPARE_COMMIT_MSG_PATH} ${current_hooks_prepare_commit_msg_path}"
 
-    gbw_print_question_yes_no "Create hooks symlink"
+    gbw_print_step "Create hooks symlink instead ($ ${command_create_symlink})"
+    gbw_print_question_yes_no "Create symlink"
 
     if [[ "${_GBW_PRINT_QUESTION_YES_NO_LAST_VALUE}" == "${GBW_PARAMS_YES}" ]]; then
         eval "${command_create_symlink}"
@@ -51,12 +50,12 @@ function gbw_install_git_hooks_symlink_create {
 
 function gbw_install_git_hooks_symlink_remove {
     local current_hooks_prepare_commit_msg_path="${1}"
-    local pcm_symlink_command_delete="rm ${current_hooks_prepare_commit_msg_path}"
+    local command_symlink_remove="rm ${current_hooks_prepare_commit_msg_path}"
 
     gbw_print_question_yes_no "Remove hooks symlink"
 
     if [[ "${_GBW_PRINT_QUESTION_YES_NO_LAST_VALUE}" == "${GBW_PARAMS_YES}" ]]; then
-        eval "${pcm_symlink_command_delete}"
+        eval "${command_symlink_remove}"
         gbw_print_step "Symlink removed"
         return
     fi
