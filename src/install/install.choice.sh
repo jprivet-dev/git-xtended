2eadce79da380dafbd873895b2d9cf26045205e8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-11 12:32:38 +0200

    dev(install.choice.sh): use gbw_install_disable_or_enable instead

--- src/install/install.choice.sh
+++ src/install/install.choice.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-function gbw_install_choice_question_yes_no {
+function gbw_install_disable_or_enable {
     local label=$1
     local key=$2
     local function_install=$3
     local choice=""
 
-    gbw_print_question_yes_no "Would you like to install '${label}'"
+    gbw_print_choose_one_option "${label}" "${GBW_PARAMS_DISABLED}" "${GBW_PARAMS_ENABLED}"
 
-    [ "${_GBW_PRINT_QUESTION_YES_NO_LAST_VALUE}" == "${GBW_PARAMS_YES}" ] && choice="${GBW_PARAMS_ON}" || choice="${GBW_PARAMS_OFF}"
+    [ "${_GBW_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}" == "${GBW_PARAMS_ENABLED}" ] && choice="${GBW_PARAMS_ON}" || choice="${GBW_PARAMS_OFF}"
 
     if [ "${function_install}" != "" -a "${choice}" == "${GBW_PARAMS_ON}" ]; then
         $function_install
@@ -18,11 +18,11 @@ function gbw_install_choice_question_yes_no {
 }
 
 function gbw_install_choice {
-    gbw_install_choice_question_yes_no "${GBW_PARAMS_LABEL_PROMPT}"        "${GBW_PARAMS_GIT_CONFIG_KEY_PROMPT}"
-    gbw_install_choice_question_yes_no "${GBW_PARAMS_LABEL_GIT_ALIASES}"   "${GBW_PARAMS_GIT_CONFIG_KEY_GIT_ALIASES}"
-    gbw_install_choice_question_yes_no "${GBW_PARAMS_LABEL_GIT_HOOKS}"     "${GBW_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS}"     "gbw_install_git_hooks"
-    gbw_install_choice_question_yes_no "${GBW_PARAMS_LABEL_WORKFLOW}"      "${GBW_PARAMS_GIT_CONFIG_KEY_WORKFLOW}"
-    gbw_install_choice_question_yes_no "${GBW_PARAMS_LABEL_BASH_ALIASES}"  "${GBW_PARAMS_GIT_CONFIG_KEY_BASH_ALIASES}"
+    gbw_install_disable_or_enable "${GBW_PARAMS_LABEL_PROMPT}"        "${GBW_PARAMS_GIT_CONFIG_KEY_PROMPT}"
+    gbw_install_disable_or_enable "${GBW_PARAMS_LABEL_GIT_ALIASES}"   "${GBW_PARAMS_GIT_CONFIG_KEY_GIT_ALIASES}"
+    gbw_install_disable_or_enable "${GBW_PARAMS_LABEL_GIT_HOOKS}"     "${GBW_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS}"     "gbw_install_git_hooks"
+    gbw_install_disable_or_enable "${GBW_PARAMS_LABEL_WORKFLOW}"      "${GBW_PARAMS_GIT_CONFIG_KEY_WORKFLOW}"
+    gbw_install_disable_or_enable "${GBW_PARAMS_LABEL_BASH_ALIASES}"  "${GBW_PARAMS_GIT_CONFIG_KEY_BASH_ALIASES}"
 }
 
 function gbw_install_choice_params_set_all_from_git_config {
