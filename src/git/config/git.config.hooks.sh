f61caac45e5dbd6b501806e45995059c0ed84bfe
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-04-13 23:26:37 +0200

    dev(git.config.hooks.sh): create gbw_git_config_hooks_set_type_hookspath & _symlink

--- src/git/config/git.config.hooks.sh
+++ src/git/config/git.config.hooks.sh
@@ -21,3 +21,11 @@ function gbw_git_config_hooks_enabled {
 function gbw_git_config_hooks_disabled {
     gbw_git_config_hooks_unset --global
 }
+
+function gbw_git_config_hooks_set_type_hookspath {
+    gwb_git_config_set "${GBW_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS_TYPE}" "${GBW_PARAMS_GIT_HOOKS_TYPE_HOOKSPATH}"
+}
+
+function gbw_git_config_hooks_set_type_symlink {
+    gwb_git_config_set "${GBW_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS_TYPE}" "${GBW_PARAMS_GIT_HOOKS_TYPE_SYMLINK}"
+}
