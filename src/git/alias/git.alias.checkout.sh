81b953d9af91cd6ff301637982028a969e590eb4
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-03 09:10:31 +0200

    dev.refactor(git.alias.checkout.sh): adjust var name

--- src/git/alias/git.alias.checkout.sh
+++ src/git/alias/git.alias.checkout.sh
@@ -4,21 +4,23 @@ indexes=$@
 
 if [ "${indexes}" == "" ]; then
     echo "which file(s) would you want to restore ?"
+    count=0
     while IFS= read -r line; do
         count=`expr ${count} + 1`
         echo -e "${count}) ${line}"
     done < <(git status --porcelain)
-else
-    status_i=0
-
-    git status -s | cut -c4- | while read path; do
-        status_i=`expr ${status_i} + 1`
-        for i in ${indexes}; do
-            if [ "${status_i}" == "${i}" ]; then
-                echo "> git checkout (${i}) ${path}"
-                git checkout ${path}
-            fi
-        done
-    done
+    exit 1
 fi
+
+status_i=0
+
+git status -s | cut -c4- | while read path; do
+    status_i=`expr ${status_i} + 1`
+    for i in ${indexes}; do
+        if [ "${status_i}" == "${i}" ]; then
+            echo "> git checkout (${i}) ${path}"
+            git checkout ${path}
+        fi
+    done
+done
 
