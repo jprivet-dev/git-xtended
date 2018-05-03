af49a34246001aac38b539f3bd05c886628d83e8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-03 12:39:15 +0200

    dev(git.alias.status.sh): active colors

--- src/git/alias/git.alias.status.sh
+++ src/git/alias/git.alias.status.sh
@@ -4,4 +4,4 @@ count=0
 while IFS= read -r line; do
     count=`expr ${count} + 1`
     echo -e "${count}) ${line}"
-done < <(git status --porcelain)
+done < <(git -c color.status=always status --short)
