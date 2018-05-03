34513b6f3e17d32ca3bff0b7bead6aa9e93c507f
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-03 08:04:13 +0200

    dev.refactor(git.config.aliases.sh): use ${global} instead of Â$global

--- src/git/config/git.config.aliases.sh
+++ src/git/config/git.config.aliases.sh
@@ -5,23 +5,23 @@ function gbw_git_config_set_aliases {
     [[ "${global}" != "--global" ]] && global=""
 
     # git branch
-    git config $global alias.b branch
+    git config ${global} alias.b branch
 
     # git diff
-    git config $global alias.d '!f() { '${GBW_PARAMS_GIT_ALIAS_DIFF_FILE}' "$1"; }; f'
+    git config ${global} alias.d '!f() { '${GBW_PARAMS_GIT_ALIAS_DIFF_FILE}' "$1"; }; f'
 
     # git add
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_A} "add"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_ALL} "add --all"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_NM} "add ."
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_MD} "add --update"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_A} "add"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_ALL} "add --all"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_NM} "add ."
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_MD} "add --update"
 
     # git commit
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_C} '!f() { '${GBW_PARAMS_GIT_ALIAS_COMMIT_FILE}' "$@"; }; f'
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_AMEND} "commit -m --amend"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_C} '!f() { '${GBW_PARAMS_GIT_ALIAS_COMMIT_FILE}' "$@"; }; f'
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_AMEND} "commit -m --amend"
 
     # git checkout
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_CK} checkout
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_CK} checkout
 
     # git log
     local hash="%C(yellow)%h"
@@ -32,52 +32,52 @@ function gbw_git_config_set_aliases {
 
     local command_log_oneline="log --graph --oneline --decorate --date=short --pretty=format:'${hash} ${time} ${author}${reference} ${subject}'"
 
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_L} "${command_log_oneline} -12"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_LL} "${command_log_oneline}"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_LFILE} "${command_log_oneline} --follow"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_LMERGES} "${command_log_oneline} --merges"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_LCOUNT} "shortlog -sn"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_L} "${command_log_oneline} -12"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_LL} "${command_log_oneline}"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_LFILE} "${command_log_oneline} --follow"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_LMERGES} "${command_log_oneline} --merges"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_LCOUNT} "shortlog -sn"
 
     # git reset
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_R} "reset"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_HARD} "reset --hard"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_UNDO} "reset --soft HEAD^"
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_UNTRACKED} "clean -f -d"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_R} "reset"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_HARD} "reset --hard"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_UNDO} "reset --soft HEAD^"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_UNTRACKED} "clean -f -d"
 
     # git status
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_S} "!sh -c ${GBW_PARAMS_GIT_ALIAS_STATUS_FILE}"
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_S} "!sh -c ${GBW_PARAMS_GIT_ALIAS_STATUS_FILE}"
 
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_SS} status
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_SS} status
 
     # git grep
-    git config $global alias.${GBW_PARAMS_GIT_ALIAS_FIND} '!f() { '${GBW_PARAMS_GIT_ALIAS_FIND_FILE}' "$@"; }; f'
+    git config ${global} alias.${GBW_PARAMS_GIT_ALIAS_FIND} '!f() { '${GBW_PARAMS_GIT_ALIAS_FIND_FILE}' "$@"; }; f'
 }
 
 function gbw_git_config_unset_aliases {
     local global="$1"
     [[ "${global}" != "--global" ]] && global=""
 
-    git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_B}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_D}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_A}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_ALL}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_NM}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_MD}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_C}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_AMEND}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_CK}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_L}}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_LL}
-    git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_LFILE}
-    git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_LMERGES}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_LCOUNT}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_UNTRACKED}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_R}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_HARD}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_UNDO}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_S}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_SS}
-	git config $global --unset alias.${GBW_PARAMS_GIT_ALIAS_FIND}
+    git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_B}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_D}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_A}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_ALL}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_NM}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_MD}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_C}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_AMEND}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_CK}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_L}}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_LL}
+    git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_LFILE}
+    git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_LMERGES}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_LCOUNT}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_UNTRACKED}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_R}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_HARD}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_UNDO}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_S}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_SS}
+	git config ${global} --unset alias.${GBW_PARAMS_GIT_ALIAS_FIND}
 }
 
 function gbw_git_aliases_enabled {
