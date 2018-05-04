9e7eb063b44e4c53b0708f524f8e189bf801de20
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-04 16:12:30 +0200

    dev.clean(src/): remove "@test" tags

--- src/git/commands/git.commands.sh
+++ src/git/commands/git.commands.sh
@@ -9,7 +9,6 @@ function gbw_git_current_folder_is_repo {
     git rev-parse --git-dir 2> /dev/null
 }
 
-# @test
 function gbw_git_get_current_branch {
     git rev-parse --abbrev-ref HEAD 2> /dev/null
 }
@@ -18,7 +17,6 @@ function gbw_git_get_current_user_name {
     git config user.name
 }
 
-# @test
 function gbw_git_get_remote_branch_ref {
     local branch="$1"
 
@@ -30,7 +28,6 @@ function gbw_git_get_remote_branch_ref {
     echo "${GBW_PARAMS_GIT_REMOTE_BRANCH_REF}"
 }
 
-# @test
 function gbw_git_status {
     git status --porcelain 2> /dev/null
 }
@@ -50,17 +47,14 @@ function gbw_git_status_indexed {
     source "${GBW_PARAMS_GIT_ALIAS_STATUS_FILE}"
 }
 
-# @test
 function gbw_git_get_changes_nb {
     git status --porcelain | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_diff_current_branch_origin_dev {
     git diff --stat "`gbw_git_get_remote_branch_ref`" 2> /dev/null
 }
 
-# @test
 function gbw_git_get_status_changes_to_be_committed {
     git status --porcelain | grep '^[^? ]' 2> /dev/null
 }
@@ -69,12 +63,10 @@ function gbw_git_get_status_changes_to_be_committed_count {
     git status --porcelain | grep '^[^? ]' | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_get_status_changes_to_be_committed_modified_count {
     git status --porcelain | grep '^M' | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_get_status_changes_to_be_committed_modified_extended_count {
     # ' ' = unmodified
     # M = modified / A = added / D = deleted
@@ -84,34 +76,28 @@ function gbw_git_get_status_changes_to_be_committed_modified_extended_count {
     git status --porcelain | grep '^[^DA? ]' | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_get_status_changes_to_be_committed_deleted_count {
     git status --porcelain | grep '^D' | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_get_status_changes_to_be_committed_new_file_count {
     git status --porcelain | grep '^A' | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_get_status_changes_not_staged_for_commit_count {
     git status --porcelain | grep '^.[^? ]' | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_get_status_untracked_files_count {
     git status --porcelain | grep '^??' | wc -l 2> /dev/null
 }
 
-# @test
 function gbw_git_status_ahead {
     local from_branch=$1
     local to_branch=$2
     git rev-list --left-right --count "${from_branch}"..."${to_branch}" 2> /dev/null
 }
 
-# @test
 function gbw_git_status_ahead_count {
     local from_branch=$1
     local to_branch=$2
@@ -121,7 +107,6 @@ function gbw_git_status_ahead_count {
     gbw_git_status_ahead "${from_branch}" "${to_branch}" | while read -a array; do echo ${array[0]} ; done
 }
 
-# @test
 function gbw_git_status_behind {
     local from_branch=$1
     local to_branch=$2
@@ -129,7 +114,6 @@ function gbw_git_status_behind {
     git rev-list --left-right --count "${to_branch}"..."${from_branch}" 2> /dev/null
 }
 
-# @test
 function gbw_git_status_behind_count {
     local from_branch=$1
     local to_branch=$2
