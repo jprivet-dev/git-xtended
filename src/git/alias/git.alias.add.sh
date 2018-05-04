54c10c6d32f1673f51637b42394e8cb367c0cbaf
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-04 15:02:31 +0200

    dev(git.alias.add.sh): create base of file

---
+++ src/git/alias/git.alias.add.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+indexes=$@
+
+if [ "${indexes}" == "" ]; then
+    printf "which file(s) would you want to add ?\n"
+    source ~/git-bash-workflow/src/git/alias/git.alias.status.sh
+    exit 1
+fi
+
+status_i=0
+
+git status -s | cut -c4- | while read path; do
+    status_i=`expr ${status_i} + 1`
+    for i in ${indexes}; do
+        if [ "${status_i}" == "${i}" ]; then
+            printf "> git add (%s) %s\n" ${i} ${path}
+            git add ${path}
+        fi
+    done
+done
+
