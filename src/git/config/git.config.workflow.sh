b998b7dc61f2b4ed7dae3c702af5bb7a7123f20f
jprivet <jprivet@groupe361.com>
2018-05-09 15:17:30 +0200

    dev.clean(all): disable workflow part

--- src/git/config/git.config.workflow.sh
+++ src/git/config/git.config.workflow.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # TODO: complete action
-function gx_git_config_workflow_enabled {
-    local nope="on"
-}
+#function gx_git_config_workflow_enabled {
+#    local nope="on"
+#}
 
 # TODO: complete action
-function gx_git_config_workflow_disabled {
-    local nope="off"
-}
+#function gx_git_config_workflow_disabled {
+#    local nope="off"
+#}
