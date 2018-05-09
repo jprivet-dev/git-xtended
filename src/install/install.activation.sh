fd735d6d281fa6ad0344f7e366641c75714ea6d9
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-09 23:27:54 +0200

    dev.fix(install.activation.sh): Prohibit usage of A && B || C as if-then-else

--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -3,9 +3,25 @@
 function gx_install_activation {
     gx_install_choice_params_set_all_from_git_config
 
-    [ "${GX_PARAMS_INSTALL_PROMPT_STATUS}" == "${GX_PARAMS_ENABLED}" ]         && gx_prompt_enabled
-    [ "${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS}" == "${GX_PARAMS_ENABLED}" ]    && gx_git_aliases_enabled           || gx_git_aliases_disabled
-    [ "${GX_PARAMS_INSTALL_GIT_HOOKS_STATUS}" == "${GX_PARAMS_ENABLED}" ]      && gx_git_config_hooks_enabled      || gx_git_config_hooks_disabled
+    if [ "${GX_PARAMS_INSTALL_PROMPT_STATUS}" == "${GX_PARAMS_ENABLED}" ]
+    then
+        gx_prompt_enabled
+    fi
+
+    if [ "${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS}" == "${GX_PARAMS_ENABLED}" ]
+    then
+        gx_git_aliases_enabled
+    else
+        gx_git_aliases_disabled
+    fi
+
+    if [ "${GX_PARAMS_INSTALL_GIT_HOOKS_STATUS}" == "${GX_PARAMS_ENABLED}" ]
+    then
+        gx_git_config_hooks_enabled
+    else
+        gx_git_config_hooks_disabled
+    fi 
+
 #    [ "${GX_PARAMS_INSTALL_WORKFLOW_STATUS}" == "${GX_PARAMS_ENABLED}" ]       && gx_git_config_workflow_enabled   || gx_git_config_workflow_disabled
 #    [ "${GX_PARAMS_INSTALL_BASH_ALIASES_STATUS}" == "${GX_PARAMS_ENABLED}" ]   && gx_bash_aliases_enabled          || gx_bash_aliases_disabled
 }
