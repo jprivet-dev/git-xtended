de1eb5a4aac5c806a8f15d4964b37219d1567d1c
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-09 14:49:36 +0200

    dev.refactor(all): use 'GX' instead of 'GBW'

--- tests/commands.tests.sh
+++ tests/commands.tests.sh
@@ -4,8 +4,8 @@ function test_gx_command_help {
     local count_lines="`gx_command_help | wc -l`"
     assert equals "${count_lines}" "9" $LINENO
 
-    assert first-line   "`gx_command_help`"   "${TEST_C_WHITE}(6) GBW commands${TEST_F_RESET} :" $LINENO
-    assert last-line    "`gx_command_help`"   "${TEST_GX_PARAMS_TAB}gx ${TEST_C_LIGHT_YELLOW}test${TEST_F_RESET} [<func>] ${TEST_C_DARK_GRAY}...${TEST_F_RESET} Run one or all GBW tests" $LINENO
+    assert first-line   "`gx_command_help`"   "${TEST_C_WHITE}(6) GX commands${TEST_F_RESET} :" $LINENO
+    assert last-line    "`gx_command_help`"   "${TEST_GX_PARAMS_TAB}gx ${TEST_C_LIGHT_YELLOW}test${TEST_F_RESET} [<func>] ${TEST_C_DARK_GRAY}...${TEST_F_RESET} Run one or all GX tests" $LINENO
 }
 
 function test_gx_command_help_command_line {
