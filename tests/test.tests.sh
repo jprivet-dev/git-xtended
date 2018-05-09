490ab71d0362c417f24b470decb17c64267e3e0a
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-09 23:04:21 +0200

    dev.fix(tests): prohibit backtick substitution

--- tests/test.tests.sh
+++ tests/test.tests.sh
@@ -2,6 +2,6 @@
 
 function test_gx_test_fake_git {
     local git_no_fake_command="git no fake command"
-    assert equals "`git no fake command`" "[ ERROR !!! No Git fake command for '${git_no_fake_command}' ]" $LINENO
+    assert equals "$(git no fake command)" "[ ERROR !!! No Git fake command for '${git_no_fake_command}' ]" $LINENO
     assert last-git-command-is "${git_no_fake_command}" $LINENO
 }
