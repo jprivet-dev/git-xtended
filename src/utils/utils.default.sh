0ddde04bbb6b746148772457edb66341988d74a8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-09 23:08:33 +0200

    dev.fix(src/utils): prohibit backtick substitution

--- src/utils/utils.default.sh
+++ src/utils/utils.default.sh
@@ -16,7 +16,7 @@ function gx_implode {
 
     for p in "${pieces[@]}"
     do
-        p="`gx_trim "${p}"`"
+        p="$(gx_trim "${p}")"
 
         if [[ "${implode}" == "" ]]; then
             [[ "${p}" != "" ]] && implode="${p}"
