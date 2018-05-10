a4a6e9a5a6025c2ad0fa336c90affce8d86a15ae
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-10 08:39:49 +0200

    dev.refactor(git.alias.status.sh): remove unnecessary ${}

--- src/git/alias/git.alias.status.sh
+++ src/git/alias/git.alias.status.sh
@@ -2,6 +2,6 @@
 
 count=0
 while IFS= read -r line; do
-    count=$((${count} + 1))
+    count=$((count + 1))
     printf "%s) %s\n" ${count} "${line}"
 done < <(git -c color.status=always status --short)
