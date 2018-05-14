0b6ad1e48771bcfcacfbcb71a5084b62ee5d0a1e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-14 14:16:46 +0200

    dev.refactor(git.hooks.pcmsg.XXX.sh): move elements in src/git/hooks party

--- src/git/hooks/git.hooks.pcmsg.params.sh
+++ src/git/hooks/git.hooks.pcmsg.params.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
+_GX_HOOKS_PCMSG_TYPE_LABEL="${C_LIGHT_MAGENTA}<type*>${F_RESET}"
+_GX_HOOKS_PCMSG_SUBTYPE_LABEL="${C_LIGHT_RED}<subtype>${F_RESET}"
+_GX_HOOKS_PCMSG_MAINSCOPE_LABEL="${C_LIGHT_GREEN}<scope*>${F_RESET}"
+_GX_HOOKS_PCMSG_SUBJECT_LABEL="${C_LIGHT_YELLOW}<subject*>${F_RESET}"
+
+_GX_HOOKS_PCMSG_TYPES=(
+    'a/archi/Changes application architecture (folder position, name, ...)'
+    'c/conf/Changes configuration, parameters (config.yml, .gitignore, .gitkeep, ...)'
+    'co/content/Changes wording or images in contents'
+    'ch/chore/Changes to the build process or auxiliary tools and libraries (Gulp, Composer, ...)'
+    'd/dev/Changes main code (creation, enhancement, evolution, ...)'
+    'do/docs/Documentation only changes'
+    "i/integ/Web integration, UX works - doesn't touch business logic (css, js, ...)"
+    "in/install/Install or update project, modules, pluggins (ex: composer install ...)"
+    't/tpl/Templating works (Twig, Drupal templating, Smarty, ...)'
+    'tr/trans/Adds locale-specific components, translates text, ...'
+)
+
+_GX_HOOKS_PCMSG_SUBTYPES=(
+    'c/clean/Remove pointless code or files'
+    'd/debug/Adding code for debugging (console.log, dpm, var_dump, ...), to remove later !'
+    'f/fix/Bug fix'
+    'g/generate/Auto-generate code (Doctrine, PhoneGap, Ionic, ...)'
+    'p/perf/A code change that improves performance'
+    'r/refactor/A code change that neither fixes a bug nor adds a feature'
+    's/style/Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, ...)'
+)
 
