49943e9ec5f371566c51a6d34cf7cdc21c7427d3
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-14 13:42:59 +0200

    dev(git.sh): import git.hooks.sh

--- src/git/git.sh
+++ src/git/git.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-source ~/git-xtended/src/git/config/git.config.sh
 source ~/git-xtended/src/git/commands/git.commands.sh
+source ~/git-xtended/src/git/config/git.config.sh
+source ~/git-xtended/src/git/hooks/git.hooks.sh
