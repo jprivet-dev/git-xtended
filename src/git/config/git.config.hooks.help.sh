2518074791f23ed3719602f16ea60b999e2a008c
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-18 23:51:47 +0200

    archi.clean(git.config.xxx.sh): remove empty files

--- src/git/config/git.config.hooks.help.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#function gx_git_config_hooks_help {
-#    gx_print_colors "${C_WHITE}${GX_PARAMS_LABEL_GIT_HOOKS}${F_RESET} :"
-#    gx_print "${GX_PARAMS_TAB}XX ... WIP"
-#    gx_print
-#}
--- src/git/config/git.config.workflow.help.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#function gx_git_config_workflow_help {
-#    gx_print_colors "${C_WHITE}${GX_PARAMS_LABEL_WORKFLOW}${F_RESET} :"
-#    gx_print "${GX_PARAMS_TAB}XX ... WIP"
-#    gx_print
-#}
