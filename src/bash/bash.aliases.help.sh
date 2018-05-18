826cb373f610262b67d41a85e7cedec4b949fdd0
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-18 23:55:14 +0200

    archi.clean(bash): remove unused files

--- src/bash/bash.aliases.help.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#function gx_bash_aliases_help {
-#    gx_print_colors "${C_WHITE}${GX_PARAMS_LABEL_BASH_ALIASES}${F_RESET} :"
-#    gx_print "${GX_PARAMS_TAB}XX ... WIP"
-#    gx_print
-#}
-
