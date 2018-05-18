09ff61cccd4d15b4f5141fccd0d38ad6ed130fc3
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-05-18 23:48:06 +0200

    dev.clean(install.activation.sh): remove unused options

--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -14,16 +14,6 @@ function gx_install_activation {
     else
         gx_git_aliases_disabled
     fi
-
-#    if [ "${GX_PARAMS_INSTALL_GIT_HOOKS_STATUS}" == "${GX_PARAMS_ENABLED}" ]
-#    then
-#        gx_git_config_hooks_enabled
-#    else
-#        gx_git_config_hooks_disabled
-#    fi
-
-#    [ "${GX_PARAMS_INSTALL_WORKFLOW_STATUS}" == "${GX_PARAMS_ENABLED}" ]       && gx_git_config_workflow_enabled   || gx_git_config_workflow_disabled
-#    [ "${GX_PARAMS_INSTALL_BASH_ALIASES_STATUS}" == "${GX_PARAMS_ENABLED}" ]   && gx_bash_aliases_enabled          || gx_bash_aliases_disabled
 }
 
 function gx_install_activation_status_color {
@@ -35,9 +25,6 @@ function gx_install_activation_show_status {
 
     local gx_prompt_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_PROMPT_STATUS})"
     local gx_git_aliases_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS})"
-#    local gx_git_hooks_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_GIT_HOOKS_STATUS})"
-#    local gx_workflow_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_WORKFLOW_STATUS})"
-#    local gx_bash_aliases_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_BASH_ALIASES_STATUS})"
 
     local line_info="- ${GX_PARAMS_GITHUB} -"
     local col_width=${#line_info}
@@ -56,25 +43,9 @@ function gx_install_activation_show_status {
     echo "${line}"
     gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_LABEL_PROMPT}"        "${gx_prompt_status}"
     gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_LABEL_GIT_ALIASES}"   "${gx_git_aliases_status}"
-#    gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_LABEL_GIT_HOOKS}"     "${gx_git_hooks_status}"
-#    gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_LABEL_WORKFLOW}"      "${gx_workflow_status}"
-#    gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_LABEL_BASH_ALIASES}"  "${gx_bash_aliases_status}"
 
     echo "${line}"
     echo "${line_info}"
     echo "${line}"
     echo
-
-#    echo "git config ..."
-#    echo "${GX_PARAMS_TAB}user.name  = $(git config user.name)"
-#    echo "${GX_PARAMS_TAB}user.email = $(git config user.email)"
-#    echo
-#
-#    echo "git config --global ..."
-#    echo "${GX_PARAMS_TAB}user.name  = $(git config --global user.name)"
-#    echo "${GX_PARAMS_TAB}user.email = $(git config --global user.email)"
-#    echo
-#
-#    echo "${line}"
-#    echo
 }
