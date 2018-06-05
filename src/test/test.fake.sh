dd92e74491ae2c55a6ae9d27171813a3231a6e0b
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-06-05 15:27:40 +0200

    dev.refactor(test.fake.sh): use TEST_GX_PARAMS_FAKE_CURRENT_BRANCH instead

--- src/test/test.fake.sh
+++ src/test/test.fake.sh
@@ -17,13 +17,13 @@ function gx_test_fake_git_status_porcelain {
 function gx_test_fake_git_branch {
     echo "  fake-branch-1"
     echo "  fake-branch-2"
-    echo "* ${TEST_GX_PARAMS_FAKE_GIT_GET_CURRENT_BRANCH}"
+    echo "* ${TEST_GX_PARAMS_FAKE_CURRENT_BRANCH}"
     echo "  develop"
     echo "  master"
 }
 
 function gx_test_fake_git_rev_parse_abbrev_ref_head {
-    echo "${TEST_GX_PARAMS_FAKE_GIT_GET_CURRENT_BRANCH}"
+    echo "${TEST_GX_PARAMS_FAKE_CURRENT_BRANCH}"
 }
 
 function gx_test_fake_git_diff_current_branch_origin_dev {
@@ -90,16 +90,16 @@ function gx_test_fake_git {
         "diff --stat ${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF}")
             gx_test_fake_git_diff_current_branch_origin_dev
         ;;
-        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_GIT_GET_CURRENT_BRANCH}...${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF}")
+        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_CURRENT_BRANCH}...${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF}")
             gx_test_fake_git_revlist_current_remote
         ;;
-        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF}...${TEST_GX_PARAMS_FAKE_GIT_GET_CURRENT_BRANCH}")
+        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF}...${TEST_GX_PARAMS_FAKE_CURRENT_BRANCH}")
             gx_test_fake_git_revlist_remote_current
         ;;
-        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_GIT_GET_CURRENT_BRANCH}...${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF_NOK}")
+        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_CURRENT_BRANCH}...${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF_NOK}")
             gx_test_fake_git_revlist_current_remote_nok
         ;;
-        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF_NOK}...${TEST_GX_PARAMS_FAKE_GIT_GET_CURRENT_BRANCH}")
+        "rev-list --left-right --count ${TEST_GX_PARAMS_FAKE_GIT_REMOTE_BRANCH_REF_NOK}...${TEST_GX_PARAMS_FAKE_CURRENT_BRANCH}")
             gx_test_fake_git_revlist_remote_current_nok
         ;;
         "rev-parse --git-dir")
