f48012ff308efaed7c5280a3fdc05862672b7b1b
jprivet-dev <jean.philippe.rivet@gmail.com>
2021-09-30 14:18:20 +0200

    feat(git.config.aliases): createpr > use remote main elements

--- src/git/alias/git.alias.createpr.sh
+++ src/git/alias/git.alias.createpr.sh
@@ -8,12 +8,19 @@ source ~/git-xtended/src/utils/utils.print.sh
 
 current_remote_origin_url=$(gx_git_get_current_remote_origin_url)
 current_remote_origin_url_last_element=${current_remote_origin_url##*@} # get the string after '@'
-url_root="https://${current_remote_origin_url_last_element/:/\/}" # change ':' by '/'
-url_root="${url_root/.git/}" # remove '.git'
-current_branch=$(gx_git_get_current_branch)
+url_root="https://${current_remote_origin_url_last_element/:/\/}"       # change ':' by '/'
+url_root="${url_root/.git/}"                                            # remove '.git'
+url_root="${url_root}/compare/"
+
+remote_main=$(gx_git_get_remote_main)
+remote_main_last_element=${remote_main##*/} # get the string after '@'
+
 remote_branch_ref=$(gx_git_get_remote_branch_ref)
 remote_branch_ref_last_element=${remote_branch_ref##*/} # get the string after '@'
 
+current_branch=$(gx_git_get_current_branch)
+
 printf "> Compare & Create PR:\n"
-printf "> ${url_root}/compare/${remote_branch_ref_last_element}...${current_branch}\n"
+printf "> ${url_root}${remote_main_last_element}...${current_branch}\n"
+printf "> ${url_root}${remote_branch_ref_last_element}...${current_branch}\n"
 printf "\n"
