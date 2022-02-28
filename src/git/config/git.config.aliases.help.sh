5d84f7098e5977c4c3e806c1dc117a900e73f480
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-02-28 13:20:29 +0100

    feat(all): adjust colors & style (PS1 & help)

--- src/git/config/git.config.aliases.help.sh
+++ src/git/config/git.config.aliases.help.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function gx_git_config_aliases_help() {
-    local select_file="(select file with ${F_UNDERLINED}index${F_RESET} status instead of path)"
+    local select_file="(select file with index status instead of path)"
 
     gx_print_colors "${C_WHITE}  # ${GX_PARAMS_LABEL_GIT_ALIASES}${F_RESET} :"
     gx_git_config_aliases_help_line "     log |" "${GX_PARAMS_GIT_ALIAS_L}" "" .................... "Show the last 12 commit logs (graphical representation)"
@@ -10,15 +10,15 @@ function gx_git_config_aliases_help() {
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_LMERGES}" "" .............. "Show all merge logs (graphical representation)"
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_LCOUNT}" "" ............... "Number of commits per author"
     gx_git_config_aliases_help_line "  branch |" "${GX_PARAMS_GIT_ALIAS_B}" "" .................... "Simple \"branch\" alias"
-    gx_git_config_aliases_help_line "checkout |" "${GX_PARAMS_GIT_ALIAS_CANCEL}" "[<i>|<path>]" .. "Extended \"checkout\" command ${select_file}"
+    gx_git_config_aliases_help_line "checkout |" "${GX_PARAMS_GIT_ALIAS_CANCEL}" "[<i>|<path>]" .. "${F_UNDERLINED}Indexed${F_RESET} \"checkout\" command ${select_file}"
     gx_git_config_aliases_help_line "  status |" "${GX_PARAMS_GIT_ALIAS_S}" "" .................... "${F_UNDERLINED}Indexed${F_RESET} status list (staged, unstaged, and untracked files)"
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_SS}" "" ................... "Simple \"status\" alias"
-    gx_git_config_aliases_help_line "    diff |" "${GX_PARAMS_GIT_ALIAS_D}" "[<i>|<path>]" ....... "Extended \"diff\" command ${select_file}"
-    gx_git_config_aliases_help_line "     add |" "${GX_PARAMS_GIT_ALIAS_A}" "[<i>|<path>]" ....... "Extended \"add\" command ${select_file}"
+    gx_git_config_aliases_help_line "    diff |" "${GX_PARAMS_GIT_ALIAS_D}" "[<i>|<path>]" ....... "${F_UNDERLINED}Indexed${F_RESET} \"diff\" command ${select_file}"
+    gx_git_config_aliases_help_line "     add |" "${GX_PARAMS_GIT_ALIAS_A}" "[<i>|<path>]" ....... "${F_UNDERLINED}Indexed${F_RESET} \"add\" command ${select_file}"
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_ALL}" "" .................. "Add all files (new, modified and deleted)"
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_NM}" "" ................... "Add new and modified files, without deleted"
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_MD}" "" ................... "Add modified and deleted files, without new"
-    gx_git_config_aliases_help_line "  commit |" "${GX_PARAMS_GIT_ALIAS_C}" "[<i>|<path>|all]" ... "Extended \"commit\" command ${select_file}"
+    gx_git_config_aliases_help_line "  commit |" "${GX_PARAMS_GIT_ALIAS_C}" "[<i>|<path>|all]" ... "${F_UNDERLINED}Indexed${F_RESET} \"commit\" command ${select_file}"
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_AMEND}" "" ................ "Modify message of the previous commit"
     gx_git_config_aliases_help_line "          " "${GX_PARAMS_GIT_ALIAS_NOEDIT}" "" ............... "Modify previous commit without modifying the commit message"
     gx_git_config_aliases_help_line "   reset |" "${GX_PARAMS_GIT_ALIAS_R}" "" .................... "Simple \"reset\" alias"
@@ -33,5 +33,5 @@ function gx_git_config_aliases_help() {
 function gx_git_config_aliases_help_line() {
     local option
     [ "$3" == "" ] && option=" " || option=" $3 "
-    gx_print_colors "${GX_PARAMS_TAB}$1 git ${C_LIGHT_CYAN}$2${F_RESET}${option}${C_DARK_GRAY}$4${F_RESET} $5"
+    gx_print_colors "${GX_PARAMS_TAB}$1 git ${C_LIGHT_BLUE}$2${F_RESET}${option}${C_DARK_GRAY}$4${F_RESET} $5"
 }
