8e74eb0377390eb4b3abcd0c018835c41d565e2b
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-03-15 12:49:00 +0100

    style(install): ide autoformat

--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 
 function gx_install_activation() {
-    gx_install_choice_params_set_all_from_git_config
+  gx_install_choice_params_set_all_from_git_config
 
-    if [ "${GX_PARAMS_INSTALL_PROMPT_STATUS}" == "${GX_PARAMS_ENABLED}" ]; then
-        gx_prompt_enabled
-    else
-        gx_prompt_disabled
-    fi
+  if [ "${GX_PARAMS_INSTALL_PROMPT_STATUS}" == "${GX_PARAMS_ENABLED}" ]; then
+    gx_prompt_enabled
+  else
+    gx_prompt_disabled
+  fi
 
-    if [ "${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS}" == "${GX_PARAMS_ENABLED}" ]; then
-        gx_git_aliases_enabled
-    else
-        gx_git_aliases_disabled
-    fi
+  if [ "${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS}" == "${GX_PARAMS_ENABLED}" ]; then
+    gx_git_aliases_enabled
+  else
+    gx_git_aliases_disabled
+  fi
 }
 
 function gx_install_activation_status_color() {
-    [ "$1" == "${GX_PARAMS_ENABLED}" ] && echo "${GX_PARAMS_ENABLED_C}" || echo "${GX_PARAMS_DISABLED_C}"
+  [ "$1" == "${GX_PARAMS_ENABLED}" ] && echo "${GX_PARAMS_ENABLED_C}" || echo "${GX_PARAMS_DISABLED_C}"
 }
 
 function gx_install_activation_show_status() {
-    gx_install_choice_params_set_all_from_git_config
+  gx_install_choice_params_set_all_from_git_config
 
-    local gx_prompt_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_PROMPT_STATUS})"
-    local gx_git_aliases_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS})"
+  local gx_prompt_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_PROMPT_STATUS})"
+  local gx_git_aliases_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS})"
 
-    local line_info="- ${GX_PARAMS_GITHUB} -"
-    local col_width=${#line_info}
-    local space=" "
+  local line_info="- ${GX_PARAMS_GITHUB} -"
+  local col_width=${#line_info}
+  local space=" "
 
-    local line=$(gx_print_fixed_width $col_width "-" "" "")
+  local line=$(gx_print_fixed_width $col_width "-" "" "")
 
-    echo
-    cat ~/git-xtended/src/logo/logo.txt
-    echo
+  echo
+  cat ~/git-xtended/src/logo/logo.txt
+  echo
 
-    echo
-    gx_print_fixed_width $col_width " " "" "${GX_PARAMS_VERSION}"
+  echo
+  gx_print_fixed_width $col_width " " "" "${GX_PARAMS_VERSION}"
 
-    echo "${line}"
-    gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_PROMPT_TITLE}"      "${gx_prompt_status}"
-    gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_GIT_ALIASES_TITLE}" "${gx_git_aliases_status}"
+  echo "${line}"
+  gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_PROMPT_TITLE}" "${gx_prompt_status}"
+  gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_GIT_ALIASES_TITLE}" "${gx_git_aliases_status}"
 
-    echo "${line}"
-    echo "${line_info}"
-    echo "${line}"
-    echo
+  echo "${line}"
+  echo "${line_info}"
+  echo "${line}"
+  echo
 }
--- src/install/install.choice.sh
+++ src/install/install.choice.sh
@@ -1,58 +1,58 @@
 #!/usr/bin/env bash
 
 function gx_install_disable_or_enable() {
-    local label=$1
-    local key=$2
-    local call_function=$3
-    local choice=""
+  local label=$1
+  local key=$2
+  local call_function=$3
+  local choice=""
 
-    gx_print_choose_one_option "${label}" "${GX_PARAMS_DISABLED}" "${GX_PARAMS_ENABLED}"
+  gx_print_choose_one_option "${label}" "${GX_PARAMS_DISABLED}" "${GX_PARAMS_ENABLED}"
 
-    [ "${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}" == "${GX_PARAMS_ENABLED}" ] && choice="${GX_PARAMS_ENABLED}" || choice="${GX_PARAMS_DISABLED}"
+  [ "${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}" == "${GX_PARAMS_ENABLED}" ] && choice="${GX_PARAMS_ENABLED}" || choice="${GX_PARAMS_DISABLED}"
 
-    if [ "${call_function}" != "" ]; then
-        $call_function "${choice}"
-    fi
+  if [ "${call_function}" != "" ]; then
+    $call_function "${choice}"
+  fi
 
-    gx_git_config_set "${key}" "${choice}"
+  gx_git_config_set "${key}" "${choice}"
 }
 
 function gx_install_input_text() {
-    local label=$1
-    local key=$2
-    local default_value=$3
+  local label=$1
+  local key=$2
+  local default_value=$3
 
-    gx_print_input_text "${label}" "${default_value}"
+  gx_print_input_text "${label}" "${default_value}"
 
-    gx_git_config_set "${key}" "${_GX_PRINT_INPUT_TEXT_LAST_VALUE}"
+  gx_git_config_set "${key}" "${_GX_PRINT_INPUT_TEXT_LAST_VALUE}"
 }
 
 function gx_install_choice() {
-    gx_install_disable_or_enable "${GX_PARAMS_PROMPT_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_PROMPT_STATUS}"
+  gx_install_disable_or_enable "${GX_PARAMS_PROMPT_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_PROMPT_STATUS}"
+  printf "\n"
+
+  if [ "${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}" == "${GX_PARAMS_ENABLED}" ]; then
+    gx_install_input_text "${GX_PARAMS_REMOTE_REF_BRANCH_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_GIT_REMOTE_REF_BRANCH}" "${GX_PARAMS_REMOTE_REF_BRANCH_DEFAULT_VALUE}"
     printf "\n"
+  fi
 
-    if [ "${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}" == "${GX_PARAMS_ENABLED}" ]; then
-        gx_install_input_text "${GX_PARAMS_REMOTE_REF_BRANCH_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_GIT_REMOTE_REF_BRANCH}" "${GX_PARAMS_REMOTE_REF_BRANCH_DEFAULT_VALUE}"
-        printf "\n"
-    fi
+  gx_install_disable_or_enable "${GX_PARAMS_GIT_ALIASES_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_GIT_ALIASES_STATUS}"
+  printf "\n"
 
-    gx_install_disable_or_enable "${GX_PARAMS_GIT_ALIASES_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_GIT_ALIASES_STATUS}"
+  if [ "${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}" == "${GX_PARAMS_ENABLED}" ]; then
+    gx_install_input_text "${GX_PARAMS_GIT_AUTOLINK_REFERENCE_PREFIX_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_GIT_AUTOLINK_REFERENCE_PREFIX}" "${GX_PARAMS_GIT_AUTOLINK_REFERENCE_PREFIX_DEFAULT_VALUE}"
     printf "\n"
-
-    if [ "${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}" == "${GX_PARAMS_ENABLED}" ]; then
-        gx_install_input_text "${GX_PARAMS_GIT_AUTOLINK_REFERENCE_PREFIX_TITLE}" "${GX_PARAMS_GIT_CONFIG_KEY_GIT_AUTOLINK_REFERENCE_PREFIX}" "${GX_PARAMS_GIT_AUTOLINK_REFERENCE_PREFIX_DEFAULT_VALUE}"
-        printf "\n"
-    fi
-    #    gx_install_disable_or_enable "${GX_PARAMS_LABEL_GIT_HOOKS}"     "${GX_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS_STATUS}"     "gx_install_git_hooks"
-    #    gx_install_disable_or_enable "${GX_PARAMS_LABEL_WORKFLOW}"      "${GX_PARAMS_GIT_CONFIG_KEY_WORKFLOW_STATUS}"
-    #    gx_install_disable_or_enable "${GX_PARAMS_LABEL_BASH_ALIASES}"  "${GX_PARAMS_GIT_CONFIG_KEY_BASH_ALIASES_STATUS}"
+  fi
+  #    gx_install_disable_or_enable "${GX_PARAMS_LABEL_GIT_HOOKS}"     "${GX_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS_STATUS}"     "gx_install_git_hooks"
+  #    gx_install_disable_or_enable "${GX_PARAMS_LABEL_WORKFLOW}"      "${GX_PARAMS_GIT_CONFIG_KEY_WORKFLOW_STATUS}"
+  #    gx_install_disable_or_enable "${GX_PARAMS_LABEL_BASH_ALIASES}"  "${GX_PARAMS_GIT_CONFIG_KEY_BASH_ALIASES_STATUS}"
 }
 
 # shellcheck disable=SC2034
 function gx_install_choice_params_set_all_from_git_config() {
-    GX_PARAMS_INSTALL_PROMPT_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_PROMPT_STATUS} ${GX_PARAMS_DISABLED})"
-    GX_PARAMS_INSTALL_GIT_ALIASES_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_GIT_ALIASES_STATUS} ${GX_PARAMS_DISABLED})"
-    #    GX_PARAMS_INSTALL_GIT_HOOKS_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS_STATUS} ${GX_PARAMS_DISABLED})"
-    #    GX_PARAMS_INSTALL_WORKFLOW_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_WORKFLOW_STATUS} ${GX_PARAMS_DISABLED})"
-    #    GX_PARAMS_INSTALL_BASH_ALIASES_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_BASH_ALIASES_STATUS} ${GX_PARAMS_DISABLED})"
+  GX_PARAMS_INSTALL_PROMPT_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_PROMPT_STATUS} ${GX_PARAMS_DISABLED})"
+  GX_PARAMS_INSTALL_GIT_ALIASES_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_GIT_ALIASES_STATUS} ${GX_PARAMS_DISABLED})"
+  #    GX_PARAMS_INSTALL_GIT_HOOKS_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_GIT_HOOKS_STATUS} ${GX_PARAMS_DISABLED})"
+  #    GX_PARAMS_INSTALL_WORKFLOW_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_WORKFLOW_STATUS} ${GX_PARAMS_DISABLED})"
+  #    GX_PARAMS_INSTALL_BASH_ALIASES_STATUS="$(gx_git_config_get ${GX_PARAMS_GIT_CONFIG_KEY_BASH_ALIASES_STATUS} ${GX_PARAMS_DISABLED})"
 }
