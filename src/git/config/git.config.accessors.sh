91774991de7596c19d9dd2be10793f3d4adc6792
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-03-15 12:47:11 +0100

    style(git.config): ide autoformat

--- src/git/config/git.config.accessors.sh
+++ src/git/config/git.config.accessors.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 
 function gx_git_config_get() {
-    local key=$1
-    local default=$2
+  local key=$1
+  local default=$2
 
-    local value="$(git config --global ${key})"
+  local value="$(git config --global ${key})"
 
-    if [ "${value}" == "" ]; then
-        echo "${default}"
-        return
-    fi
+  if [ "${value}" == "" ]; then
+    echo "${default}"
+    return
+  fi
 
-    echo "${value}"
+  echo "${value}"
 }
 
 function gx_git_config_set() {
-    git config --global $1 $2
+  git config --global $1 $2
 }
 
 function gx_git_config_set_global_alias_cmd() {
-    local name="$1"
-    shift
+  local name="$1"
+  shift
 
-    git config --global alias.$name \"$*\"
+  git config --global alias.$name \"$*\"
 }
 
 function gx_git_config_set_global_alias_func_cmd() {
-    git config --global alias.$1 "\"!f() { $2; }; f\""
+  git config --global alias.$1 "\"!f() { $2; }; f\""
 }
 
 function gx_git_config_set_global_alias_sh_cmd() {
-    git config --global alias.$1 "!sh -c $2"
+  git config --global alias.$1 "!sh -c $2"
 }
