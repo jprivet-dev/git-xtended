82f92449813e4c3940c23734fbb0aad4c642a822
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-03-15 13:00:05 +0100

    style(gx): ide autoformat

--- gx.sh
+++ gx.sh
@@ -5,9 +5,9 @@ GX_ENV_TEST=${GX_ENV_TEST:-0}
 source ~/git-xtended/params/default/params.sh
 
 if [[ "${GX_ENV_TEST}" == 1 ]]; then
-    source ~/git-xtended/params/test/params.test.sh
+  source ~/git-xtended/params/test/params.test.sh
 elif [[ -f ~/git-xtended/params.sh ]]; then
-    source ~/git-xtended/params.sh
+  source ~/git-xtended/params.sh
 fi
 
 source ~/git-xtended/src/utils/utils.sh
