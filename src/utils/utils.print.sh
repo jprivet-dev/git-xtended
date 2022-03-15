1db84311df8187a5b6774dde410a86595f4c84f5
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-03-15 12:53:16 +0100

    style(utils): ide autoformat

--- src/utils/utils.print.sh
+++ src/utils/utils.print.sh
@@ -1,190 +1,190 @@
 #!/usr/bin/env bash
 
 function gx_print_fixed_width() {
-    local e="${GX_PARAMS_OFF}"
-    local width middle_char start end
-    local start_no_style end_no_style
-    local middle_prefix middle_suffix
-    local middle trucate count
+  local e="${GX_PARAMS_OFF}"
+  local width middle_char start end
+  local start_no_style end_no_style
+  local middle_prefix middle_suffix
+  local middle trucate count
 
-    if [ "$1" == "-e" ]; then
-        e="${GX_PARAMS_ON}"
-        shift
-    fi
+  if [ "$1" == "-e" ]; then
+    e="${GX_PARAMS_ON}"
+    shift
+  fi
 
-    width="$1"
-    middle_char="$2"
-    start="$3"
-    end="$4"
-    [ "$5" != "" ] && middle_prefix="$5"
-    [ "$6" != "" ] && middle_suffix="$6"
+  width="$1"
+  middle_char="$2"
+  start="$3"
+  end="$4"
+  [ "$5" != "" ] && middle_prefix="$5"
+  [ "$6" != "" ] && middle_suffix="$6"
 
-    if [ "${e}" == "${GX_PARAMS_ON}" ]; then
-        start_no_style=$(gx_regex_remove_colors "${start}")
-        end_no_style=$(gx_regex_remove_colors "${end}")
-    else
-        start_no_style="${start}"
-        end_no_style="${end}"
-    fi
+  if [ "${e}" == "${GX_PARAMS_ON}" ]; then
+    start_no_style=$(gx_regex_remove_colors "${start}")
+    end_no_style=$(gx_regex_remove_colors "${end}")
+  else
+    start_no_style="${start}"
+    end_no_style="${end}"
+  fi
 
-    middle=$(gx_line_generator "${width}" "${middle_char}")
-    count=${#start_no_style}+${#end_no_style}+${#middle_prefix}+${#middle_suffix}
-    trucate="${middle_prefix}${middle:$count}${middle_suffix}"
+  middle=$(gx_line_generator "${width}" "${middle_char}")
+  count=${#start_no_style}+${#end_no_style}+${#middle_prefix}+${#middle_suffix}
+  trucate="${middle_prefix}${middle:$count}${middle_suffix}"
 
-    if [ "${e}" == "${GX_PARAMS_ON}" ]; then
-        echo -e "${start}${trucate}${end}"
-        return
-    fi
+  if [ "${e}" == "${GX_PARAMS_ON}" ]; then
+    echo -e "${start}${trucate}${end}"
+    return
+  fi
 
-    echo "${start}${trucate}${end}"
+  echo "${start}${trucate}${end}"
 }
 
 function gx_print_col_fixed_width() {
-    local width=$1
-    local text=$2
+  local width=$1
+  local text=$2
 
-    if [[ "${width}" < 1 ]]; then
-        width=1
-    fi
+  if [[ "${width}" < 1 ]]; then
+    width=1
+  fi
 
-    local text_length=${#text}
-    local spaces=""
+  local text_length=${#text}
+  local spaces=""
 
-    for ((i = text_length; i < width; i++)); do
-        spaces="${spaces} "
-    done
+  for ((i = text_length; i < width; i++)); do
+    spaces="${spaces} "
+  done
 
-    printf "${text}${spaces}"
+  printf "${text}${spaces}"
 }
 
 function gx_print() {
-    echo "$*"
+  echo "$*"
 }
 
 function gx_print_title_1() {
-    gx_print_colors_force "${C_BG_YELLOW}${C_BLACK}\n\n $*\n${F_RESET}\n"
+  gx_print_colors_force "${C_BG_YELLOW}${C_BLACK}\n\n $*\n${F_RESET}\n"
 }
 
 function gx_print_title_2() {
-    gx_print_colors_force "${C_LIGHT_YELLOW}\n--- $* ---\n${F_RESET}"
+  gx_print_colors_force "${C_LIGHT_YELLOW}\n--- $* ---\n${F_RESET}"
 }
 
 function gx_print_title_success() {
-    gx_print_colors_force "${C_BG_GREEN}${C_WHITE}\n\n $*\n${F_RESET}\n"
+  gx_print_colors_force "${C_BG_GREEN}${C_WHITE}\n\n $*\n${F_RESET}\n"
 }
 
 function gx_print_title_error() {
-    gx_print_colors_force "${C_BG_RED}${C_WHITE}\n\n $*\n${F_RESET}\n"
+  gx_print_colors_force "${C_BG_RED}${C_WHITE}\n\n $*\n${F_RESET}\n"
 }
 
 function gx_print_step() {
-    gx_print_colors_force "> $*"
+  gx_print_colors_force "> $*"
 }
 
 function gx_print_colors() {
-    echo ${GX_PARAMS_PRINT_E_TAG} "$*"
+  echo ${GX_PARAMS_PRINT_E_TAG} "$*"
 }
 
 function gx_print_colors_force() {
-    echo -e "$*"
+  echo -e "$*"
 }
 
 function gx_print_question_yes_no() {
-    local choice
-    local label="$*"
+  local choice
+  local label="$*"
 
-    while true; do
-        echo -e -n "${C_CYAN}>${F_RESET} ${C_BG_CYAN}${C_BLACK} ${label} ${F_RESET}? [y/n] "
+  while true; do
+    echo -e -n "${C_CYAN}>${F_RESET} ${C_BG_CYAN}${C_BLACK} ${label} ${F_RESET}? [y/n] "
 
-        exec </dev/tty
-        read choice
+    exec </dev/tty
+    read choice
 
-        if [ "${choice}" == "${GX_PARAMS_Y}" -o "${choice}" == "${GX_PARAMS_N}" ]; then
-            break
-        fi
-    done
-
-    if [ "${choice}" == "y" ]; then
-        _GX_PRINT_QUESTION_YES_NO_LAST_VALUE="${GX_PARAMS_YES}"
-        return
+    if [ "${choice}" == "${GX_PARAMS_Y}" -o "${choice}" == "${GX_PARAMS_N}" ]; then
+      break
     fi
+  done
 
-    # shellcheck disable=SC2034
-    _GX_PRINT_QUESTION_YES_NO_LAST_VALUE="${GX_PARAMS_NO}"
+  if [ "${choice}" == "y" ]; then
+    _GX_PRINT_QUESTION_YES_NO_LAST_VALUE="${GX_PARAMS_YES}"
+    return
+  fi
+
+  # shellcheck disable=SC2034
+  _GX_PRINT_QUESTION_YES_NO_LAST_VALUE="${GX_PARAMS_NO}"
 }
 
 function gx_print_choose_one_option() {
-    local choice
-    local label="$1"
-    shift
+  local choice
+  local label="$1"
+  shift
 
-    local -a options="($*)"
-    local count="0"
-    local o
+  local -a options="($*)"
+  local count="0"
+  local o
 
-    local choice_index
-    local choice_label
+  local choice_index
+  local choice_label
 
-    local message="${C_CYAN}>${F_RESET} ${C_BG_CYAN}${C_BLACK} ${label} ${F_RESET}"
+  local message="${C_CYAN}>${F_RESET} ${C_BG_CYAN}${C_BLACK} ${label} ${F_RESET}"
+
+  for o in "${options[@]}"; do
+    message="${message}  ${C_LIGHT_CYAN}[${count}]${F_RESET} ${o}"
+    ((count++))
+  done
+
+  echo -e -n "${message}"
+
+  while true; do
+    echo -n " : "
+
+    exec </dev/tty
+    read choice_index
+
+    choice_label=""
+    count="0"
 
     for o in "${options[@]}"; do
-        message="${message}  ${C_LIGHT_CYAN}[${count}]${F_RESET} ${o}"
-        ((count++))
+      if [[ "${choice_index}" == "${count}" ]]; then
+        choice_label="${o}"
+      fi
+      ((count++))
     done
 
-    echo -e -n "${message}"
+    if [[ "${choice_label}" != "" ]]; then
+      break
+    fi
+  done
 
-    while true; do
-        echo -n " : "
+  _GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE="${choice_label}"
 
-        exec </dev/tty
-        read choice_index
-
-        choice_label=""
-        count="0"
-
-        for o in "${options[@]}"; do
-            if [[ "${choice_index}" == "${count}" ]]; then
-                choice_label="${o}"
-            fi
-            ((count++))
-        done
-
-        if [[ "${choice_label}" != "" ]]; then
-            break
-        fi
-    done
-
-    _GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE="${choice_label}"
-
-    gx_print_step "${label} = ${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}"
+  gx_print_step "${label} = ${_GX_PRINT_CHOOSE_ONE_OPTION_LAST_VALUE}"
 }
 
 function gx_print_input_text() {
-    local choice
-    local label="$1"
-    local default_value="$2"
+  local choice
+  local label="$1"
+  local default_value="$2"
 
-    local message="${C_CYAN}>${F_RESET} ${C_BG_CYAN}${C_BLACK} ${label} ${F_RESET}"
+  local message="${C_CYAN}>${F_RESET} ${C_BG_CYAN}${C_BLACK} ${label} ${F_RESET}"
 
-    if [ "${default_value}" != "" ]; then
-      message+=" [${default_value}]"
+  if [ "${default_value}" != "" ]; then
+    message+=" [${default_value}]"
+  fi
+
+  message+=": "
+
+  while true; do
+    echo -e -n "${message}"
+
+    exec </dev/tty
+    read choice
+
+    if [ "${default_value}" != "" -o "${choice}" != "" ]; then
+      break
     fi
+  done
 
-    message+=": ";
+  _GX_PRINT_INPUT_TEXT_LAST_VALUE="${choice:-${default_value}}"
 
-    while true; do
-        echo -e -n "${message}"
-
-        exec </dev/tty
-        read choice
-
-        if [ "${default_value}" != "" -o "${choice}" != "" ]; then
-            break
-        fi
-    done
-
-    _GX_PRINT_INPUT_TEXT_LAST_VALUE="${choice:-${default_value}}"
-
-    gx_print_step "${label} = ${_GX_PRINT_INPUT_TEXT_LAST_VALUE}"
+  gx_print_step "${label} = ${_GX_PRINT_INPUT_TEXT_LAST_VALUE}"
 }
