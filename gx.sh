2da787745caa6ed4ec578fa83ff8bf3866bb51da
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-03-17 12:01:50 +0100

    feat(colors): adjust all colors & logo

--- gx.sh
+++ gx.sh
@@ -4,17 +4,18 @@ GX_ENV_TEST=${GX_ENV_TEST:-0}
 
 source ~/git-xtended/params/default/params.sh
 
-if [[ "${GX_ENV_TEST}" == 1 ]]; then
+if [ "${GX_ENV_TEST}" == 1 ]; then
   source ~/git-xtended/params/test/params.test.sh
 elif [[ -f ~/git-xtended/params.sh ]]; then
   source ~/git-xtended/params.sh
 fi
 
-source ~/git-xtended/src/utils/utils.sh
-source ~/git-xtended/src/commands/commands.sh
 source ~/git-xtended/src/colors/colors.sh
+source ~/git-xtended/src/commands/commands.sh
 source ~/git-xtended/src/git/git.sh
-source ~/git-xtended/src/prompt/prompt.sh
 source ~/git-xtended/src/install/install.sh
+source ~/git-xtended/src/logo/logo.sh
+source ~/git-xtended/src/prompt/prompt.sh
+source ~/git-xtended/src/utils/utils.sh
 
 gx_install
--- src/install/install.activation.sh
+++ src/install/install.activation.sh
@@ -25,26 +25,22 @@ function gx_install_activation_show_status() {
 
   local gx_prompt_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_PROMPT_STATUS})"
   local gx_git_aliases_status="$(gx_install_activation_status_color ${GX_PARAMS_INSTALL_GIT_ALIASES_STATUS})"
-
-  local line_info="- ${GX_PARAMS_GITHUB} -"
-  local col_width=${#line_info}
+  local col_width=$((${#GX_PARAMS_GITHUB} + 4))
   local space=" "
-
   local line=$(gx_print_fixed_width $col_width "-" "" "")
+  local line_version=$(gx_print_fixed_width $col_width " " "" "${GX_PARAMS_VERSION}")
 
-  echo
-  cat ~/git-xtended/src/logo/logo.txt
-  echo
+  printf "\n"
+  gx_logo_color
+  printf "${C_LIGHT_MAGENTA}%s${F_RESET}\n" "${line_version}"
+  printf "\n"
 
-  echo
-  gx_print_fixed_width $col_width " " "" "${GX_PARAMS_VERSION}"
+  printf "${C_DARK_GRAY}%s${F_RESET}\n" "${line}"
+  gx_print_fixed_width -e $col_width "${space}" "${C_DARK_GRAY}>${F_RESET} ${GX_PARAMS_PROMPT_TITLE}" "${gx_prompt_status}"
+  gx_print_fixed_width -e $col_width "${space}" "${C_DARK_GRAY}>${F_RESET} ${GX_PARAMS_GIT_ALIASES_TITLE}" "${gx_git_aliases_status}"
 
-  echo "${line}"
-  gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_PROMPT_TITLE}" "${gx_prompt_status}"
-  gx_print_fixed_width -e $col_width "${space}" "${GX_PARAMS_GIT_ALIASES_TITLE}" "${gx_git_aliases_status}"
-
-  echo "${line}"
-  echo "${line_info}"
-  echo "${line}"
-  echo
+  printf "${C_DARK_GRAY}%s${F_RESET}\n" "${line}"
+  printf "${C_DARK_GRAY}>${F_RESET} %s ${C_DARK_GRAY}<${F_RESET}\n" "${GX_PARAMS_GITHUB}"
+  printf "${C_DARK_GRAY}%s${F_RESET}\n" "${line}"
+  printf "\n"
 }
