2e4e0e57aa2f1beb6f52980a8751b4c0f83a078a
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-13 10:02:03 +0100

    refactor(git.config.accessors): use double quote to prevent globbing and word splitting

--- src/git/config/git.config.accessors.sh
+++ src/git/config/git.config.accessors.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 function gx_git_config_get() {
-  local key=$1
-  local default=$2
+  local key="$1"
+  local default="$2"
 
-  local value="$(git config --global ${key})"
+  local value="$(git config --global "${key}")"
 
   if [ "${value}" == "" ]; then
     echo "${default}"
@@ -15,20 +15,20 @@ function gx_git_config_get() {
 }
 
 function gx_git_config_set() {
-  git config --global $1 $2
+  git config --global "$1" "$2"
 }
 
 function gx_git_config_set_global_alias_cmd() {
   local name="$1"
   shift
 
-  git config --global alias.$name \"$*\"
+  git config --global alias."${name}" \""$*"\"
 }
 
 function gx_git_config_set_global_alias_func_cmd() {
-  git config --global alias.$1 "\"!f() { $2; }; f\""
+  git config --global alias."$1" "\"!f() { $2; }; f\""
 }
 
 function gx_git_config_set_global_alias_sh_cmd() {
-  git config --global alias.$1 "!sh -c $2"
+  git config --global alias."$1" "!sh -c $2"
 }
